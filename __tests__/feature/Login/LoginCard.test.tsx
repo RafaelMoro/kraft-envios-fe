ca8d75172dbc1a59206024c34d9b249fd7aa5d0f
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-18 23:22:27 -0600

    test: add axios mock and implement successful login redirection in LoginCard tests

--- __tests__/feature/Login/LoginCard.test.tsx
+++ __tests__/feature/Login/LoginCard.test.tsx
@@ -1,9 +1,11 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
 import userEvent from '@testing-library/user-event'
+import axios from 'axios';
 
 import { QueryProviderWrapper } from "@/app/QueryProviderWrapper"
 import { LoginCard } from "@/features/Login/LoginCard"
 import { AppRouterContextProviderMock } from "@/shared/ui/organisms/AppRouterContextProviderMock"
+import { DASHBOARD_ROUTE } from "@/shared/constants/global.constants";
 
 const LoginCardWrapper = ({
   push,
@@ -22,6 +24,9 @@ const LoginCardWrapper = ({
     </QueryProviderWrapper>
   )
 }
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('LoginCard', () => {
   it('Show Login Card', () => {
@@ -81,6 +86,47 @@ describe('LoginCard', () => {
       const signInButton = screen.getByRole('button', { name: /iniciar sesión/i })
       await user.click(signInButton)
       expect(await screen.findByText(/Por favor, ingrese su contraseña/i))
+    })
+  })
+
+  describe('Form submission', () => {
+    it('Given a user entering email or password correctly, redirect to dashboard', async () => {
+      const toggleNotification = jest.fn()
+      const updateNotificationMessage = jest.fn()
+      const push = jest.fn()
+      const user = userEvent.setup()
+      mockedAxios.post.mockResolvedValue({
+        error: null,
+        message: null,
+        success: true,
+        version: "v1.2.0",
+        data: {
+          user: {
+            _id: "some-id",
+            email: "a-new-usero@mail.com",
+            firstName: "john",
+            lastName: "Doe",
+          }
+        }
+      })
+
+      render(
+        <LoginCardWrapper
+          push={push}
+          toggleNotification={toggleNotification}
+          updateNotificationMessage={updateNotificationMessage}
+        />
+      )
+
+      const pwdInput = screen.getByLabelText(/contraseña/i)
+      await user.type(pwdInput, '123')
+      const emailInput = screen.getByLabelText(/correo electrónico/i)
+      await user.type(emailInput, 'correo-electronico@a.com')
+      const signInButton = screen.getByRole('button', { name: /iniciar sesión/i })
+      await user.click(signInButton)
+      await waitFor(() => {
+        expect(push).toHaveBeenCalledWith(DASHBOARD_ROUTE)
+      }, { timeout: 2000 })
     })
   })
 })
