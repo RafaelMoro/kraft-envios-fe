54cffe195628a99db9633212fe88ac8f22b94f80
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-20 01:10:01 -0600

    feat: enhance ResetPasswordCard with validation and error handling for password fields

--- src/features/Login/ResetPassword/ResetPasswordCard.tsx
+++ src/features/Login/ResetPassword/ResetPasswordCard.tsx
@@ -1,6 +1,12 @@
-import { LOGIN_ROUTE } from "@/shared/constants/global.constants"
-import { LinkButton } from "@/shared/ui/atoms/LinkButton"
+"use client"
 import { Button, Card, Label, TextInput } from "flowbite-react"
+import { yupResolver } from "@hookform/resolvers/yup"
+import { SubmitHandler, useForm } from "react-hook-form"
+
+import { LOGIN_ROUTE } from "@/shared/constants/global.constants"
+import { ResetPasswordFormData, ResetPasswordSchema } from "@/shared/types/login.types"
+import { ErrorMessage } from "@/shared/ui/atoms/ErrorMessage"
+import { LinkButton } from "@/shared/ui/atoms/LinkButton"
 
 interface ResetPasswordCardProps{
   slug: string
@@ -8,13 +14,26 @@ interface ResetPasswordCardProps{
 
 export const ResetPasswordCard = ({ slug }: ResetPasswordCardProps) => {
   console.log('slug', slug)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ResetPasswordFormData>({
+    resolver: yupResolver(ResetPasswordSchema)
+  })
+
+  const onSubmit: SubmitHandler<ResetPasswordFormData> = (data) => {
+    console.log('data', data)
+  }
+
+
   return (
     <Card className="max-w-[400px]">
       <p className="text-xl text-black dark:text-white mb-2">
         Ingresa y confirma tu nueva contraseña para recuperar el acceso.
       </p>
       <form
-        // onSubmit={handleSubmit(onSubmit)}
+        onSubmit={handleSubmit(onSubmit)}
         className="flex max-w-md flex-col gap-4"
       >
         <div>
@@ -25,11 +44,11 @@ export const ResetPasswordCard = ({ slug }: ResetPasswordCardProps) => {
             data-testid="password"
             id="password"
             type="password"
-            // {...register("password")}
+            {...register("password")}
           />
-          {/* { errors?.password?.message && (
-            <ErrorMessage isAnimated>{errors.password?.message}</ErrorMessage>
-          )} */}
+          { errors?.password?.message && (
+            <ErrorMessage>{errors.password?.message}</ErrorMessage>
+          )}
         </div>
         <div>
           <div className="mb-2 block">
@@ -39,11 +58,11 @@ export const ResetPasswordCard = ({ slug }: ResetPasswordCardProps) => {
             data-testid="confirmPassword"
             type="password"
             id="confirmPassword"
-            // {...register("confirmPassword")}
+            {...register("confirmPassword")}
           />
-          {/* { errors?.confirmPassword?.message && (
-            <ErrorMessage isAnimated>{errors.confirmPassword?.message}</ErrorMessage>
-          )} */}
+          { errors?.confirmPassword?.message && (
+            <ErrorMessage>{errors.confirmPassword?.message}</ErrorMessage>
+          )}
         </div>
         <LinkButton className="mt-4" type="secondary" href={LOGIN_ROUTE} >Volver al inicio</LinkButton>
         <Button
--- src/shared/types/login.types.ts
+++ src/shared/types/login.types.ts
@@ -1,4 +1,4 @@
-import { object, string } from "yup";
+import { object, ref, string } from "yup";
 import { ERROR_EMAIL_REQUIRED, ERROR_INVALID_EMAIL, ERROR_PASSWORD_REQUIRED } from "../constants/login.constants";
 import { AxiosError, AxiosResponse } from "axios";
 
@@ -52,10 +52,36 @@ export interface ForgotPasswordError extends Omit<AxiosError, 'response'> {
   }>;
 }
 
+// Reset Password
+export type ResetPasswordFormData = {
+  password: string
+  confirmPassword: string
+}
+
 //#region Validation schemas
 const emailRegex = /^[^@]+@[^@]+\.[^@]+$/;
 
 const emailValidation = string().email(ERROR_INVALID_EMAIL).required(ERROR_EMAIL_REQUIRED).matches(emailRegex, ERROR_INVALID_EMAIL);
+
+const passwordValidation = (requiredMessage: string, onlyRequired = false) => {
+  if (onlyRequired) return string().required(requiredMessage);
+  return string()
+    .required(requiredMessage)
+    .min(16, 'La contraseña debe tener al menos 16 caracteres. Ingrese más caracteres')
+    .max(40, 'La contraseña puede tener un máximo de 40 caracteres. Ha excedido los 40 caracteres')
+    .matches(/[A-Z]+/, 'La contraseña debe contener al menos 1 mayúscula')
+    .matches(/[a-z]+/, 'La contraseña debe contener al menos 1 minúscula')
+    .matches(/[0-9]+/, 'La contraseña debe contener al menos 1 número')
+    .matches(/^\S*$/, 'La contraseña no debe contener espacios en blanco.')
+    .matches(
+      /[!@#$%^&*()[\]{}+*\-_.,;:/<>?=`~\\|']+/,
+      'La contraseña debe contener al menos 1 caracter especial como !@#$%^&*()[]{}+*-_.,;:/<>?=`~|\\|',
+    );
+};
+
+const confirmPasswordValidation = string()
+  .required('Por favor, ingrese su contraseña nuevamente')
+  .oneOf([ref('password')], 'Contraseña y confirmar contraseña deben ser iguales.');
 
 export const LoginSchema = object({
   email: emailValidation,
@@ -64,4 +90,9 @@ export const LoginSchema = object({
 
 export const ForgotPasswordSchema = object({
   email: emailValidation
+})
+
+export const ResetPasswordSchema = object({
+  password: passwordValidation('Por favor, ingrese una contraseña'),
+  confirmPassword: confirmPasswordValidation,
 })
