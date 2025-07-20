2c33f441c77fc7999b07ea4865d6dbf7cdb1e5d6
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-20 11:33:07 -0600

    feat: update CompanyDetails component with company name, address, and postal code fields

--- src/features/Login/Register/CompanyDetails.tsx
+++ src/features/Login/Register/CompanyDetails.tsx
@@ -10,19 +10,20 @@ export const CompanyDetails = ({ goPrev }: CompanyDetailsProps) => {
       <h5 className="text-2xl font-bold text-center text-gray-900 dark:text-white">
         Crear cuenta.
       </h5>
-      <p className="text-xl text-black dark:text-white">Llene la siguiente información para crear su cuenta.</p>
+      <p className="text-xl text-black dark:text-white">Cuéntanos el nombre de tu compañía, su dirección y el código postal para empezar a trabajar juntos.</p>
       <form
         // onSubmit={handleSubmit(onSubmit)}
         className="flex max-w-md flex-col gap-4"
       >
         <div>
           <div className="mb-2 block">
-            <Label htmlFor="firstName">Nombre</Label>
+            <Label htmlFor="companyName">Nombre de la compañia</Label>
           </div>
           <TextInput
-            data-testid="firstName"
+            data-testid="companyName"
             // defaultValue={personalInformation.firstName}
-            id="firstName" type="text"
+            id="companyName"
+            type="text"
             // {...register("firstName")}
           />
           {/* { errors?.firstName?.message && (
@@ -31,10 +32,10 @@ export const CompanyDetails = ({ goPrev }: CompanyDetailsProps) => {
         </div>
         <div>
           <div className="mb-2 block">
-            <Label htmlFor="lastName">Apellido</Label>
+            <Label htmlFor="address">Dirección</Label>
           </div>
           <TextInput
-            id="lastName"
+            id="address"
             // defaultValue={personalInformation.lastName}
             type="text"
             // {...register("lastName")}
@@ -42,6 +43,17 @@ export const CompanyDetails = ({ goPrev }: CompanyDetailsProps) => {
           {/* { errors?.lastName?.message && (
             <ErrorMessage>{errors?.lastName?.message}</ErrorMessage>
           )} */}
+        </div>
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="postalCode">Código postal</Label>
+          </div>
+          <TextInput
+            data-testid="postalCode"
+            id="postalCode"
+            type="text"
+            inputMode="numeric"
+          />
         </div>
         <Button className="hover:cursor-pointer" outline onClick={goPrev}>Regresar</Button>
         <Button type="submit" className="hover:cursor-pointer">
