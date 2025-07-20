1bcaed6b578e402a5155c0fc237f2ddd04fc2563
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-20 17:03:11 -0600

    refactor: remove unused secondary success message import from ResultCard

--- src/features/Login/Register/ResultCard.tsx
+++ src/features/Login/Register/ResultCard.tsx
@@ -1,5 +1,5 @@
 import { LOGIN_ROUTE } from "@/shared/constants/global.constants";
-import { SUCCESS_CREATE_USER_MESSAGE, SUCCESS_CREATE_USER_SPAN, SUCCESS_CREATE_USER_TITLE, SUCESS_CREATE_USER_SECONDARY_MESSAGE } from "@/shared/constants/login.constants";
+import { SUCCESS_CREATE_USER_MESSAGE, SUCCESS_CREATE_USER_SPAN, SUCCESS_CREATE_USER_TITLE } from "@/shared/constants/login.constants";
 import { LinkButton } from "@/shared/ui/atoms/LinkButton";
 import { Button, Card } from "flowbite-react";
 
