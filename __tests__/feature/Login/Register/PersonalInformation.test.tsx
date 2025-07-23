252eccfa1cae56d99552ccf9b58350f8b7203202
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-07-22 21:32:46 -0600

    test: add unit tests for PersonalInformation component

---
+++ __tests__/feature/Login/Register/PersonalInformation.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react"
+
+import { PersonalInformation } from "@/features/Login/Register/PersonalInformation"
+import { PersonalInformationForm } from "@/shared/types/login.types"
+
+const PersonalInformationWrapper = ({
+  goNext,
+  updatePersonalInformation,
+  personalInformation = null
+}: {
+  goNext: () => void
+  updatePersonalInformation: (data: PersonalInformationForm) => void
+  personalInformation?: PersonalInformationForm | null
+}) => {
+  const currentPersonalInformation = personalInformation ?? {
+    name: "",
+    lastName: "",
+    phone: ""
+  }
+  return (
+    <PersonalInformation
+      goNext={goNext}
+      updatePersonalInformation={updatePersonalInformation}
+      personalInformation={currentPersonalInformation}
+    />
+  )
+}
+
+describe('PersonalInformation', () => {
+  it('Show personal information form', () => {
+    const mockGoNext = jest.fn()
+    const mockUpdatePersonalInformation = jest.fn()
+    render(<PersonalInformationWrapper goNext={mockGoNext} updatePersonalInformation={mockUpdatePersonalInformation} />)
+
+    expect(screen.getByLabelText(/name/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/last name/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/phone/i)).toBeInTheDocument()
+  })
+})
