3a3974c941abd0732414a79ecf8a67ab9c2acf76
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-10 14:12:25 -0600

    Add SIGN_OUT_ENDPOINT constant and update Dashboard component to use it for sign-out functionality

--- src/features/Dashboard/Dashboard.tsx
+++ src/features/Dashboard/Dashboard.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from 'next/navigation'
 import { Button } from "flowbite-react"
-import { LOGIN_ROUTE } from '@/shared/constants/global.constants'
+import { LOGIN_ROUTE, SIGN_OUT_ENDPOINT } from '@/shared/constants/global.constants'
 import { LoginData } from '@/shared/types/login.types'
 
 export interface DashboardProps {
@@ -13,16 +13,16 @@ export const Dashboard = ({ userInfo }: DashboardProps) => {
   const router = useRouter()
 
   const handleSignOut = async () => {
-    await fetch('/api/auth/sign-out')
+    await fetch(SIGN_OUT_ENDPOINT)
     router.push(LOGIN_ROUTE)
   }
 
   return (
-    <div className="w-full min-h-screen max-w-screen-2xl flex mx-auto my-0 p-4">
-      <aside className="w-72 border-r border-r-gray-600">
+    <div className="w-full min-h-screen max-w-screen-2xl flex mx-auto my-0">
+      <aside className="w-72 border-r border-r-gray-600 p-4">
         <Button color="red" outline onClick={handleSignOut}>Cerrar sesión</Button>
       </aside>
-      <main>
+      <main className='p-4'>
         <h1 className="text-3xl font-bold text-center">Bienvenido {userInfo?.data?.user?.name}</h1>
       </main>
     </div>
--- src/shared/constants/global.constants.ts
+++ src/shared/constants/global.constants.ts
@@ -2,6 +2,9 @@
 export const COOKIE_SESSION_KEY = 'session'
 export const COOKIE_USER_INFO_KEY = 'user-info'
 
+// API routes
+export const SIGN_OUT_ENDPOINT = '/api/auth/sign-out'
+
 // Routes
 export const REGISTER_ROUTE = '/register';
 export const FORGOT_PASSWORD_ROUTE = '/forgot-password';
