842b477bcb3e0f3a1836afa3dc74d34f5cb0b762
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-12 23:55:08 -0600

    Refactor QuoteCard component layout to improve structure and enhance readability

--- src/features/Quotes/QuoteCard.tsx
+++ src/features/Quotes/QuoteCard.tsx
@@ -11,25 +11,27 @@ interface QuoteProps {
 export const QuoteCard = ({ quote }: QuoteProps) => {
   return (
     <Card href="#" className="max-w-sm">
-      <div className="inline-flex gap-2">
-        { quote.logoSrc.source === 'paquetexpres' && (<PaqueteExpressIcon />) }
-        { quote.logoSrc.source === 'none' && (<RiTruckLine size={30} />) }
-        { quote.logoSrc.source !== 'paquetexpres' && quote.logoSrc.source !== 'none' && (
-          <Image src={quote.logoSrc.source} alt="Quote provider" width={quote.logoSrc.width} height={quote.logoSrc.height} />
-        )}
-        <h5 className="text-lg font-semibold tracking-tight text-gray-900 dark:text-white">
-          {quote.service}
-        </h5>
-      </div>
-      <div className="inline-flex gap-2">
-        <RiBuilding3Line />
-        <p className="text-sm text-gray-700 dark:text-gray-400">
-          {quote.source}
+      <div className="grid grid-cols-2 grid-rows-3 gap-y-4">
+        <div className="col-span-2 flex justify-center">
+          { quote.logoSrc.source === 'paquetexpres' && (<PaqueteExpressIcon />) }
+          { quote.logoSrc.source === 'none' && (<RiTruckLine size={30} />) }
+          { quote.logoSrc.source !== 'paquetexpres' && quote.logoSrc.source !== 'none' && (
+            <Image src={quote.logoSrc.source} alt="Quote provider" width={quote.logoSrc.width} height={quote.logoSrc.height} />
+          )}
+        </div>
+          <h5 className="text-lg tracking-wider text-gray-900 dark:text-white">
+            {quote.service}
+          </h5>
+        <p className="font-semibold text-2xl justify-self-end place-self-center">
+          {quote.amountFormatted}
         </p>
+        <div className="inline-flex gap-2">
+          <RiBuilding3Line size={20} />
+          <p className="text-xs text-gray-700 dark:text-gray-400">
+            {quote.source}
+          </p>
+        </div>
       </div>
-      <p className="font-normal text-gray-700 dark:text-gray-400">
-        {quote.amountFormatted}
-      </p>
     </Card>
   )
 }
