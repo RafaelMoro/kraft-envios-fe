47e66772cb937a59e6970ee08bbf533ef42d332f
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-13 07:42:20 -0600

    Refactor QuoteForm component layout to enhance structure and improve error handling

--- src/features/Quotes/QuoteForm.tsx
+++ src/features/Quotes/QuoteForm.tsx
@@ -43,98 +43,111 @@ export const QuoteForm = ({ updateQuotes }: QuoteFormProps) => {
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="grid grid-cols-3 grid-rows-2 gap-4"
+      className="flex flex-col justify-center gap-16"
     >
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="originPostalCode">Código Postal de Origen</Label>
+      <section className="flex flex-col gap-5">
+        <h4 className="text-xl font-semibold mb-4">Domicilio</h4>
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="originPostalCode">Código Postal de Origen</Label>
+            </div>
+            <TextInput
+              id="originPostalCode"
+              type="text"
+              inputMode="numeric"
+              {...register("originPostalCode")}
+            />
+            { errors.originPostalCode?.message && (
+              <ErrorMessage>{errors.originPostalCode?.message}</ErrorMessage>
+            )}
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="destinationPostalCode">Código Postal de Destino</Label>
+            </div>
+            <TextInput
+              id="destinationPostalCode"
+              type="text"
+              inputMode="numeric"
+              {...register("destinationPostalCode")}
+            />
+            { errors.destinationPostalCode?.message && (
+              <ErrorMessage>{errors.destinationPostalCode?.message}</ErrorMessage>
+            )}
+          </div>
         </div>
-        <TextInput
-          id="originPostalCode"
-          type="text"
-          inputMode="numeric"
-          {...register("originPostalCode")}
-        />
-        { errors.originPostalCode?.message && (
-          <ErrorMessage>{errors.originPostalCode?.message}</ErrorMessage>
-        )}
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="destinationPostalCode">Código Postal de Destino</Label>
+      </section>
+
+      <section className="flex flex-col gap-5">
+        <h4 className="text-xl font-semibold">Paquete:</h4>
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="weight">Peso</Label>
+            </div>
+            <TextInput
+              id="weight"
+              type="number"
+              required
+              {...register("weight")}
+            />
+            { errors.weight?.message && (
+              <ErrorMessage>{errors.weight?.message}</ErrorMessage>
+            )}
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="length">Largo</Label>
+            </div>
+            <TextInput
+              id="length"
+              type="number"
+              required
+              {...register("length")}
+            />
+            { errors.length?.message && (
+              <ErrorMessage>{errors.length?.message}</ErrorMessage>
+            )}
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="height">Altura</Label>
+            </div>
+            <TextInput
+              id="height"
+              type="number"
+              required
+              {...register("height")}
+            />
+            { errors.height?.message && (
+              <ErrorMessage>{errors.height?.message}</ErrorMessage>
+            )}
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="width">Ancho</Label>
+            </div>
+            <TextInput
+              id="width"
+              type="number"
+              required
+              {...register("width")}
+            />
+            { errors.width?.message && (
+              <ErrorMessage>{errors.width?.message}</ErrorMessage>
+            )}
+          </div>
         </div>
-        <TextInput
-          id="destinationPostalCode"
-          type="text"
-          inputMode="numeric"
-          {...register("destinationPostalCode")}
-        />
-        { errors.destinationPostalCode?.message && (
-          <ErrorMessage>{errors.destinationPostalCode?.message}</ErrorMessage>
-        )}
+      </section>
+      <div className="flex justify-center">
+        <Button
+          className="hover:cursor-pointer"
+          disabled={isPending}
+          type="submit">
+          { isPending ? (<Spinner aria-label="loading get quotes kraft envios" />) : 'Cotizar' }
+        </Button>
       </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="weight">Peso</Label>
-        </div>
-        <TextInput
-          id="weight"
-          type="number"
-          required
-          {...register("weight")}
-        />
-        { errors.weight?.message && (
-          <ErrorMessage>{errors.weight?.message}</ErrorMessage>
-        )}
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="length">Largo</Label>
-        </div>
-        <TextInput
-          id="length"
-          type="number"
-          required
-          {...register("length")}
-        />
-        { errors.length?.message && (
-          <ErrorMessage>{errors.length?.message}</ErrorMessage>
-        )}
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="height">Altura</Label>
-        </div>
-        <TextInput
-          id="height"
-          type="number"
-          required
-          {...register("height")}
-        />
-        { errors.height?.message && (
-          <ErrorMessage>{errors.height?.message}</ErrorMessage>
-        )}
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="width">Ancho</Label>
-        </div>
-        <TextInput
-          id="width"
-          type="number"
-          required
-          {...register("width")}
-        />
-        { errors.width?.message && (
-          <ErrorMessage>{errors.width?.message}</ErrorMessage>
-        )}
-      </div>
-      <Button
-        className="hover:cursor-pointer"
-        disabled={isPending}
-        type="submit">
-        { isPending ? (<Spinner aria-label="loading get quotes kraft envios" />) : 'Cotizar' }
-      </Button>
     </form>
   )
 }
