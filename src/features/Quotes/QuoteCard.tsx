f9095fce9f7c3133b864f042980212281147dd6e
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-14 15:48:01 -0600

    Refactor QuoteCard component to improve logo rendering logic and enhance title styling

--- src/features/Quotes/QuoteCard.tsx
+++ src/features/Quotes/QuoteCard.tsx
@@ -1,5 +1,6 @@
 import { RiBuilding3Line } from "@remixicon/react"
 import Image from "next/image"
+import clsx from "clsx"
 
 import { QuoteUI } from "@/shared/types/quotes.types"
 import { PaqueteExpressIcon } from "@/shared/ui/icons/PaqueteExpressIcon"
@@ -10,6 +11,11 @@ interface QuoteProps {
 
 export const QuoteCard = ({ quote }: QuoteProps) => {
   const isOtherProvider = quote.logoSrc.source.includes('kraft')
+  const isPaquetExpProvider = quote.logoSrc.source === 'paquetexpres'
+  const titleStyles = clsx(
+    "text-base text-gray-900 dark:text-white",
+    { "place-self-end justify-self-start": isOtherProvider && !isPaquetExpProvider }
+  )
 
   return (
     <article
@@ -18,17 +24,17 @@ export const QuoteCard = ({ quote }: QuoteProps) => {
       >
         <div className="h-full p-4 grid grid-card-quote grid-cols-3 gap-y-2">
           <div className="row-span-2 justify-self-center place-self-center">
-            { quote.logoSrc.source === 'paquetexpres' && (<PaqueteExpressIcon />) }
-            { isOtherProvider && quote.logoSrc.source !== 'paquetexpres' && (
+            { isPaquetExpProvider && (<PaqueteExpressIcon />) }
+            { isOtherProvider && !isPaquetExpProvider && (
               <picture className="flex h-28 w-28 bg-gray-100 rounded-full justify-center items-center">
                 <Image src={quote.logoSrc.source} alt="Quote provider" width={quote.logoSrc.width} height={quote.logoSrc.height} />
               </picture>
             ) }
-            { quote.logoSrc.source !== 'paquetexpres' && !isOtherProvider && (
+            { !isPaquetExpProvider && !isOtherProvider && (
               <Image src={quote.logoSrc.source} alt="Quote provider" width={quote.logoSrc.width} height={quote.logoSrc.height} />
             )}
           </div>
-          <h5 className="text-base text-gray-900 dark:text-white">
+          <h5 className={titleStyles}>
             {quote.service}
           </h5>
           <p className="row-span-2 font-semibold text-2xl justify-self-center place-self-center">
