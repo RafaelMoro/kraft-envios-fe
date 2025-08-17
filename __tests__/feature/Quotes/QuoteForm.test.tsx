659adb277e335c51d372cb57ac5369087c4c268a
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-08-16 19:32:35 -0600

    Add form submission test for QuoteForm to verify successful quote retrieval

--- __tests__/feature/Quotes/QuoteForm.test.tsx
+++ __tests__/feature/Quotes/QuoteForm.test.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import axios from 'axios';
+
 import { QueryProviderWrapper } from '@/features/QueryProviderWrapper'
 import { QuoteForm } from '@/features/Quotes/QuoteForm'
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('QuoteForm', () => {
   it('Show the fields needed for the quote form', () => {
@@ -82,6 +87,67 @@ describe('QuoteForm', () => {
         expect(screen.getByText(/El largo debe ser mayor que 0/i)).toBeInTheDocument()
         expect(screen.getByText(/La altura debe ser mayor que 0/i)).toBeInTheDocument()
         expect(screen.getByText(/El ancho debe ser mayor que 0/i)).toBeInTheDocument()
+      })
+    })
+  })
+
+  describe('Form submission', () => {
+    it('Submit successfully the quote form', async () => {
+      mockedAxios.post.mockResolvedValue({
+        error: null,
+        message: null,
+        success: true,
+        version: "v1.2.0",
+        data: {
+          data: {
+            data: {
+              quotes: [
+                {
+                  "id": "a48f570d-b45d-489f-99a3-5cfb266db69e",
+                  "service": "Estafeta Terrestre c/Recolección",
+                  "total": 225,
+                  "source": "GE"
+                }
+              ]
+            }
+          }
+        }
+      })
+      const updateQuotes = jest.fn()
+      const user = userEvent.setup()
+  
+      render(
+        <QueryProviderWrapper>
+          <QuoteForm updateQuotes={updateQuotes} />
+        </QueryProviderWrapper>
+      )
+  
+      const originInput = screen.getByLabelText(/Código Postal de Origen/i)
+      const destinationInput = screen.getByLabelText(/Código Postal de Destino/i)
+      const weightInput = screen.getByLabelText(/Peso/i)
+      const lengthInput = screen.getByLabelText(/Largo/i)
+      const heightInput = screen.getByLabelText(/Altura/i)
+      const widthInput = screen.getByLabelText(/Ancho/i)
+  
+      await user.type(originInput, '12345')
+      await user.type(destinationInput, '12345')
+      await user.type(weightInput, '10')
+      await user.type(lengthInput, '10')
+      await user.type(heightInput, '10')
+      await user.type(widthInput, '10')
+      await user.click(screen.getByRole('button', { name: /Cotizar/i }))
+  
+      await waitFor(() => {
+        expect(updateQuotes).toHaveBeenCalledWith(
+          [
+            {
+              "id": "a48f570d-b45d-489f-99a3-5cfb266db69e",
+              "service": "Estafeta Terrestre c/Recolección",
+              "source": "GE",
+              "total": 225
+            }
+          ]
+        )
       })
     })
   })
