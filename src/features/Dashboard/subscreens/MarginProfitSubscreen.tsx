349b449766c91bcbcf7c010c5678dfeca5bb544e
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-26 21:53:59 -0600

    Add ShowProfitMargin component and integrate it into MarginProfitSubscreen

--- src/features/Dashboard/subscreens/MarginProfitSubscreen.tsx
+++ src/features/Dashboard/subscreens/MarginProfitSubscreen.tsx
@@ -1,4 +1,5 @@
 import { ProfitMarginForm } from "@/features/ProfitMargin/ProfitMarginForm"
+import { ShowProfitMargin } from "@/features/ProfitMargin/ShowProfitMargin"
 import { LoginData } from "@/shared/types/login.types"
 
 interface MarginProgitSubscreenProps {
@@ -10,6 +11,7 @@ export const MarginProfitSubscreen = ({ userInfo }: MarginProgitSubscreenProps) 
     <main className='w-full p-4 flex flex-col gap-5 align-center'>
       <h1 className="text-3xl font-bold text-center">Bienvenido {userInfo?.data?.user?.name}</h1>
       <p className="text-center text-xl mb-5">Ingrese los siguientes datos para actualizar el margen de ganancia</p>
+      <ShowProfitMargin />
       <ProfitMarginForm />
     </main>
   )
---
+++ src/features/ProfitMargin/ShowProfitMargin.tsx
@@ -0,0 +1,7 @@
+export const ShowProfitMargin = () => {
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-center">Margen de ganancia actual</h1>
+    </>
+  )
+}
