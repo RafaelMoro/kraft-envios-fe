02d7954234307d1b91eccf4849ace72e1ba36e2f
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-26 22:15:46 -0600

    Refactor getMarginProfitCb to return a Promise of ProfitMargin or null

--- src/shared/utils/margin-profit.utils.ts
+++ src/shared/utils/margin-profit.utils.ts
@@ -1,6 +1,11 @@
 import axios from 'axios'
 import { GET_MARGIN_PROFIT } from '../constants/global.constants'
+import { ProfitMargin } from '../types/margin-profit.types'
 
-export const getMarginProfitCb = () => {
-  return axios.get(GET_MARGIN_PROFIT)
+export const getMarginProfitCb = async (): Promise<ProfitMargin | null> => {
+  const res = await axios.get(GET_MARGIN_PROFIT)
+  // API shape: res.data -> GetMarginProfitData
+  // GetMarginProfitData.data.profitMargin is the array we need
+  const profit: ProfitMargin = res.data?.data?.data?.profitMargin
+  return profit ?? null
 }
