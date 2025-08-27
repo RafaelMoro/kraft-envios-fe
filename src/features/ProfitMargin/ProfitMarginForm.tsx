88b8a7d243e07b0d05184bebd086c9038d941fb4
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-26 22:24:50 -0600

    Refactor ProfitMarginForm to update field labels, remove unused inputs, and add profit margin type dropdown

--- src/features/ProfitMargin/ProfitMarginForm.tsx
+++ src/features/ProfitMargin/ProfitMarginForm.tsx
@@ -1,4 +1,4 @@
-import { Button, Label, TextInput } from "flowbite-react"
+import { Button, Dropdown, DropdownItem, Label, TextInput } from "flowbite-react"
 
 export const ProfitMarginForm = () => {
   return (
@@ -7,15 +7,14 @@ export const ProfitMarginForm = () => {
       className="flex flex-col justify-center gap-16"
     >
       <section className="flex flex-col gap-5">
-        <h4 className="text-xl font-semibold mb-4">Domicilio</h4>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <h4 className="text-xl font-semibold mb-4">Actualizar margen de ganancia</h4>
           <div>
             <div className="mb-2 block">
-              <Label htmlFor="originPostalCode">Código Postal de Origen</Label>
+              <Label htmlFor="value">Valor</Label>
             </div>
             <TextInput
-              id="originPostalCode"
-              type="text"
+              id="value"
+              type="number"
               inputMode="numeric"
               // {...register("originPostalCode")}
             />
@@ -23,21 +22,10 @@ export const ProfitMarginForm = () => {
               <ErrorMessage>{errors.originPostalCode?.message}</ErrorMessage>
             )} */}
           </div>
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="destinationPostalCode">Código Postal de Destino</Label>
-            </div>
-            <TextInput
-              id="destinationPostalCode"
-              type="text"
-              inputMode="numeric"
-              // {...register("destinationPostalCode")}
-            />
-            {/* { errors.destinationPostalCode?.message && (
-              <ErrorMessage>{errors.destinationPostalCode?.message}</ErrorMessage>
-            )} */}
-          </div>
-        </div>
+          <Dropdown label="Profit margin type dropdown" inline dismissOnClick={false}>
+            <DropdownItem>Porcentaje</DropdownItem>
+            <DropdownItem>Absoluto</DropdownItem>
+          </Dropdown>
       </section>
 
       <div className="flex justify-center">
