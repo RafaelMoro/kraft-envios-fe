4dde3d5725b1b1368e7822cda4994004b6d3b8a3
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-26 22:18:14 -0600

    Refactor ShowProfitMargin component to remove unused variables and update heading to display current margin

--- src/features/ProfitMargin/ShowProfitMargin.tsx
+++ src/features/ProfitMargin/ShowProfitMargin.tsx
@@ -4,16 +4,17 @@ import { getMarginProfitCb } from "@/shared/utils/margin-profit.utils"
 import { useQuery } from "@tanstack/react-query"
 
 export const ShowProfitMargin = () => {
-  const { isPending, error, data } = useQuery({
+  const { data } = useQuery({
     queryKey: ['profitMargin'],
     queryFn: getMarginProfitCb
   })
-  console.log('data', data)
-  console.log('isPending', isPending)
+
+  const isPercentage = data?.type === 'percentage'
+  const currentMargin = isPercentage ? `${data?.value}%` : data?.value
 
   return (
     <>
-      <h1 className="text-2xl font-bold text-center">Margen de ganancia actual</h1>
+      <h2 className="text-2xl font-bold text-center">Margen de ganancia actual: {currentMargin}</h2>
     </>
   )
 }
