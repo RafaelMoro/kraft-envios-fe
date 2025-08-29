442d0c2f317c778239bc9ebb3ea42dce021f77d0
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-08-29 10:17:11 -0600

    Reorder elements in MarginProfitSubscreen and wrap current margin display in a card in ShowProfitMargin for improved layout and styling

--- src/features/Dashboard/subscreens/MarginProfitSubscreen.tsx
+++ src/features/Dashboard/subscreens/MarginProfitSubscreen.tsx
@@ -22,8 +22,8 @@ export const MarginProfitSubscreen = ({ userInfo }: MarginProgitSubscreenProps) 
   return (
     <main className='w-full p-4 flex flex-col gap-5 align-center'>
       <h1 className="text-3xl font-bold text-center">Bienvenido {userInfo?.data?.user?.name}</h1>
-      <p className="text-center text-xl mb-5">Ingrese los siguientes datos para actualizar el margen de ganancia</p>
       <ShowProfitMargin data={data} />
+      <p className="text-center text-xl mb-5">Ingrese los siguientes datos para actualizar el margen de ganancia</p>
       <ProfitMarginForm refetchMarginProfit={refetchMarginProfit} />
     </main>
   )
--- src/features/ProfitMargin/ShowProfitMargin.tsx
+++ src/features/ProfitMargin/ShowProfitMargin.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import { ProfitMargin } from "@/shared/types/margin-profit.types"
+import { RiLineChartLine } from "@remixicon/react"
+import { Card } from "flowbite-react"
 
 interface ShowProfitMarginProps {
   data: ProfitMargin | null | undefined
@@ -13,7 +15,16 @@ export const ShowProfitMargin = ({ data }: ShowProfitMarginProps) => {
 
   return (
     <>
-      <h2 className="text-2xl font-bold text-center">Margen de ganancia actual: {currentMargin}</h2>
+      <Card href="#" className="max-w-sm">
+        <h2 className="text-2xl font-bold text-center tracking-tight">Margen de ganancia actual</h2>
+        <div className="text-green-700 dark:text-green-400 flex justify-center gap-2">
+          { isPercentage && <RiLineChartLine /> }
+          <p className="text-lg">
+            +
+            {currentMargin}
+          </p>
+        </div>
+      </Card>
     </>
   )
 }
