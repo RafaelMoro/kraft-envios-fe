b5020eb189fafddba0a1992e676a925c3609b322
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-09-03 13:44:59 -0600

    Refactor ProfitMarginForm layout to enhance structure and add visual cues for adding couriers

--- src/features/ProfitMargin/ProfitMarginForm.tsx
+++ src/features/ProfitMargin/ProfitMarginForm.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useState } from "react"
-import { Button, Dropdown, DropdownItem, Label, Spinner, TextInput } from "flowbite-react"
+import { Button, Card, Dropdown, DropdownItem, Label, Spinner, TextInput } from "flowbite-react"
 import { SubmitHandler, useForm } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup"
 import { useMutation } from "@tanstack/react-query"
@@ -10,7 +10,7 @@ import { ErrorMessage } from "@/shared/ui/atoms/ErrorMessage"
 import { updateMarginProfitCb } from "@/shared/utils/margin-profit.utils"
 import { GeneralApiError } from "@/shared/types/global.types"
 import { QUOTE_COURIERS, QUOTE_SOURCES, QuoteCourier, QuoteSource } from "@/shared/types/quotes.types"
-import { RiAddLine } from "@remixicon/react"
+import { RiAddLine, RiArchiveLine } from "@remixicon/react"
 
 interface ProfitMarginFormProps {
   refetchMarginProfit: () => Promise<void>
@@ -52,23 +52,23 @@ export const ProfitMarginForm = ({ refetchMarginProfit, data }: ProfitMarginForm
       onSubmit={handleSubmit(onSubmit)}
       className="flex flex-col w-full justify-center gap-12 mx-auto"
     >
-      <section className="flex flex-col gap-3">
-        <h4 className="text-xl font-semibold mb-4 text-center">Configuración por proveedor</h4>
+      <div className="flex flex-col gap-3">
+        <h4 className="text-2xl font-semibold mb-4 text-center">Configuración por proveedor</h4>
         <p className="text-center text-gray-600 dark:text-gray-400">Configure los margenes de ganancia por paquetería y por proveedor</p>
-        <div className="flex gap-3 items-center justify-center">
-          <div className="mb-2 block">
-            <Label htmlFor="provider-value" className="text-lg">Proveedor:</Label>
-          </div>
-          <Dropdown label={selectedProvider} inline>
-            {allProviders.map((provider) => (
-              <DropdownItem key={provider} onClick={() => updateProvider(provider)}>
-                {provider}
-              </DropdownItem>
-            ))}
-          </Dropdown>
-        </div>
-        <article className="flex flex-col md:flex-row justify-between mt-10">
-          <h5 className="text-lg font-semibold mb-4 text-center">Configuración de la paquetería</h5>
+      </div>
+      <Card className="mx-auto w-full">
+          <Label htmlFor="provider-value" className="text-xl">Seleccione el proveedor:</Label>
+        <Dropdown label={selectedProvider}>
+          {allProviders.map((provider) => (
+            <DropdownItem key={provider} onClick={() => updateProvider(provider)}>
+              {provider}
+            </DropdownItem>
+          ))}
+        </Dropdown>
+      </Card>
+      <Card className="mx-auto w-full">
+        <div className="flex flex-col md:flex-row justify-between">
+          <h5 className="text-xl font-semibold mb-4 text-center">Configuración de la paquetería</h5>
           <Button
             className="hover:cursor-pointer inline-flex gap-1"
             color="light"
@@ -76,8 +76,15 @@ export const ProfitMarginForm = ({ refetchMarginProfit, data }: ProfitMarginForm
             <RiAddLine />
             Agregar paquetería
           </Button>
-        </article>
-      </section>
+        </div>
+        <div className="flex flex-col gap-3 justify-center items-center mt-10">
+          <span className="text-gray-600 dark:text-gray-400">
+            <RiArchiveLine size={40} />
+          </span>
+          <p className="text-lg text-center">No ha agregado ninguna paquetería</p>
+          <p className="text-sm text-gray-600 dark:text-gray-400">De click en &quot;Agregar paquetería&quot; para añadir una nueva configuración.</p>
+        </div>
+      </Card>
 
       <div className="flex justify-center">
         <Button
