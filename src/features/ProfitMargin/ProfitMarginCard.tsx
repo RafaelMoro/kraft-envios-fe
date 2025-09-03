56ac455137310c34128d7909d3c365d083b113b1
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-09-02 21:42:57 -0600

    Refactor ProfitMarginCard and ShowProfitMargin components to streamline data handling and improve readability

--- src/features/ProfitMargin/ProfitMarginCard.tsx
+++ src/features/ProfitMargin/ProfitMarginCard.tsx
@@ -1,28 +1,34 @@
-import { CourierGlobalConfig } from "@/shared/types/margin-profit.types"
-import { QuoteSource } from "@/shared/types/quotes.types"
+import { ProviderGlobalConfig } from "@/shared/types/margin-profit.types"
 import { RiLineChartLine } from "@remixicon/react"
 import { Card } from "flowbite-react"
 
 interface ProfitMarginCardProps {
-  source: QuoteSource
-  courierInfo: CourierGlobalConfig
+  providerInfo: ProviderGlobalConfig
 }
 
-export const ProfitMarginCard = ({ source, courierInfo }: ProfitMarginCardProps) => {
-  const isPercentage = courierInfo?.profitMargin?.type === 'percentage'
-  const value = courierInfo?.profitMargin?.value ?? null
-  const currentMargin = isPercentage ? `${value}%` : `$${value}`
+export const ProfitMarginCard = ({ providerInfo }: ProfitMarginCardProps) => {
 
   return (
     <Card href="#" className="max-w-sm mx-auto">
-      <span className="text-sm text-center tracking-tight text-gray-600 dark:text-gray-400">Origen: {source}</span>
-      <h4 className="text-2xl font-bold text-center tracking-tight">{courierInfo?.name}</h4>
-      <div className="text-green-700 dark:text-green-400 flex justify-center gap-2">
-        { isPercentage && <RiLineChartLine /> }
-        <p className="text-lg">
-          +
-          {currentMargin}
-        </p>
+      <span className="text-sm text-center tracking-tight text-gray-600 dark:text-gray-400">Origen: {providerInfo.name}</span>
+      <h4 className="text-2xl font-bold text-center tracking-tight">Proveedores:</h4>
+      <div className="grid grid-cols-2 gap-2">
+        { providerInfo.couriers.map((courier) => {
+          const isPercentage = courier?.profitMargin?.type === 'percentage'
+          const value = courier?.profitMargin?.value ?? null
+          const currentMargin = isPercentage ? `${value}%` : `$${value}`
+
+          return (
+            <div key={courier.name} className="flex gap-2">
+              <span className="text-lg text-center">{courier.name}</span>
+              { isPercentage && <span className="text-green-700 dark:text-green-400"><RiLineChartLine /></span> }
+              <p className="text-base text-green-700 dark:text-green-400">
+                +
+                {currentMargin}
+              </p>
+            </div>
+          )
+        })}
       </div>
     </Card>
   )
--- src/features/ProfitMargin/ShowProfitMargin.tsx
+++ src/features/ProfitMargin/ShowProfitMargin.tsx
@@ -9,25 +9,21 @@ interface ShowProfitMarginProps {
 
 export const ShowProfitMargin = ({ data }: ShowProfitMarginProps) => {
 
-  // if (!value) {
-  //   return (
-  //     <section className="flex flex-col gap-4">
-  //       <h2 className="text-2xl font-bold text-center tracking-tight">Margen de ganancia no establecido</h2>
-  //       <p className="text-center text-gray-600 dark:text-gray-400">No se ha establecido un margen de ganancia aún.</p>
-  //     </section>
-  //   )
-  // }
+  if (!data || data.length === 0) {
+    return (
+      <section className="flex flex-col gap-4">
+        <h2 className="text-2xl font-bold text-center tracking-tight">Margen de ganancia no establecido</h2>
+        <p className="text-center text-gray-600 dark:text-gray-400">No se ha establecido un margen de ganancia aún.</p>
+      </section>
+    )
+  }
 
   return (
-    <div>
-      { (data && data.length > 0) && (
-        data.map((prov) => {
-          const source = prov.name
-          return prov.couriers.map((courier) => (
-            <ProfitMarginCard key={courier.name} source={source} courierInfo={courier} />
-          ))
-        })
-      ) }
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      { (data && data.length > 0) && data.map((provider) => (
+        <ProfitMarginCard key={provider.name} providerInfo={provider} />
+      ))
+      }
     </div>
   )
 }
