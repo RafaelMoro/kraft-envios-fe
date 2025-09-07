5022a2d5752dca4d1525b8dd849cecb9edb6925c
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-09-07 09:51:15 -0600

    fix: update button CSS class for selected state in DashboardAsideLink

--- src/features/Dashboard/DashboardAsideLink.tsx
+++ src/features/Dashboard/DashboardAsideLink.tsx
@@ -10,7 +10,7 @@ interface DashboardAsideLinkProps {
 export const DashboardAsideLink = ({ onClickCb, children, isSelected }: DashboardAsideLinkProps) => {
   const buttonCss = clsx(
     "text-gray-600 dark:text-gray-400 hover:text-indigo-600 dark:hover:text-indigo-400 flex items-center gap-x-2.5 rounded-md px-2 py-1.5 text-sm transition hover:bg-gray-200 dark:hover:bg-gray-900  focus-visible:outline-2",
-    { "dark:text-indigo-400": isSelected }
+    { "text-indigo-600 dark:text-indigo-400": isSelected }
   )
 
   return (
