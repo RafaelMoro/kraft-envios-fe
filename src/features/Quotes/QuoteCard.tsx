3c1e849a02edb2df1d7c051852b1d8cf3ac44aa6
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-09-07 11:02:21 -0600

    fix: adjust layout and styling in QuoteCard for improved responsiveness

--- src/features/Quotes/QuoteCard.tsx
+++ src/features/Quotes/QuoteCard.tsx
@@ -16,7 +16,7 @@ export const QuoteCard = ({ quote }: QuoteProps) => {
   const isFedexProvider = quote.logoSrc.provider === 'fedex'
 
   const titleStyles = clsx(
-    "col-span-2 text-base text-gray-900 dark:text-white",
+    "col-span-3 md:col-span-5 lg:col-span-8 text-base text-gray-900 dark:text-white",
     { "place-self-end justify-self-start": isOtherProvider || isFedexProvider }
   )
 
@@ -25,8 +25,8 @@ export const QuoteCard = ({ quote }: QuoteProps) => {
         data-testid="quote-img"
         className="flex rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800 flex-col hover:bg-gray-100 dark:hover:bg-gray-700 cursor-pointer"
       >
-        <div className="h-full p-4 grid grid-card-quote grid-cols-3 gap-y-2">
-          <div className="row-span-2 justify-self-center place-self-center">
+        <div className="h-full p-4 grid grid-cols-4  md:grid-cols-8 lg:grid-cols-12 gap-y-2 gap-x-3 md:gap-x-0">
+          <div className="md:col-span-3 row-span-2 place-self-center">
             { isPaquetExpProvider && (<PaqueteExpressIcon />) }
             { isFedexProvider && (
               <picture className="flex h-20 md:h-28 w-20 md:w-28 bg-gray-800 rounded-full justify-center items-center">
@@ -45,10 +45,10 @@ export const QuoteCard = ({ quote }: QuoteProps) => {
           <h5 className={titleStyles}>
             {quote.service}
           </h5>
-          <p className="md:row-span-2 font-semibold text-2xl justify-self-start md:justify-self-center place-self-center">
+          <p className="md:col-span-2 lg:col-span-4 md:row-span-2 font-semibold text-2xl">
             {quote.amountFormatted}
           </p>
-          <div className="inline-flex gap-2 text-gray-700 dark:text-gray-400 justify-self-end md:justify-self-start">
+          <div className="md:col-span-2 inline-flex gap-2 text-gray-700 dark:text-gray-400 justify-self-end">
             <RiBuilding3Line size={20} />
             <p className="text-xs">
               {quote.source}
