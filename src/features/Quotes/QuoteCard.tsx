409d8d996c6f95f4aef4094d2f8ea7624f52db30
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-09-13 08:49:37 -0600

    feat: refactor QuoteCard layout for improved readability and structure

--- src/features/Quotes/QuoteCard.tsx
+++ src/features/Quotes/QuoteCard.tsx
@@ -34,42 +34,42 @@ export const QuoteCard = ({ quote, addSelectedQuote, removeSelectedQuote }: Quot
 
   return (
     <article
-        data-testid="quote-img"
-        className="flex rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800 flex-col hover:bg-gray-100 dark:hover:bg-gray-700"
-      >
-        <div className="h-full p-4 grid grid-cols-4  md:grid-cols-8 lg:grid-cols-12 gap-y-2 gap-x-3 lg:gap-x-2">
-          <div className="row-span-2 flex justify-center items-center cursor-pointer">
-            <Checkbox className="cursor-pointer" onChange={(event) => handleCheckboxChange(event)} />
-          </div>
-          <div className="md:col-span-2 lg:col-span-3 row-span-2 place-self-center">
-            { isPaquetExpProvider && (<PaqueteExpressIcon />) }
-            { isFedexProvider && (
-              <picture className="flex h-18 lg:h-24 w-18 lg:w-24 bg-gray-800 rounded-full justify-center items-center">
-                <Image src={quote.logoSrc.source} alt="Fedex provider" width={quote.logoSrc.width} height={quote.logoSrc.height} />
-              </picture>
-            ) }
-            { (isOtherProvider || is99Provider) && (
-              <picture className="flex h-16 w-16 md:h-24 md:w-24 dark:bg-gray-100 rounded-full justify-center items-center">
-                <Image src={quote.logoSrc.source} alt="Other provider" width={quote.logoSrc.width} height={quote.logoSrc.height} />
-              </picture>
-            ) }
-            { (!isPaquetExpProvider && !isOtherProvider && !isFedexProvider && !is99Provider) && (
-              <Image src={quote.logoSrc.source} alt="Quote provider" width={quote.logoSrc.width} height={quote.logoSrc.height} />
-            )}
-          </div>
-          <h5 className={titleStyles}>
-            {quote.service}
-          </h5>
-          <p className="md:col-span-2 lg:col-span-3 md:row-span-2 font-semibold text-2xl">
-            {quote.amountFormatted}
-          </p>
-          <div className="md:col-span-2 inline-flex gap-2 text-gray-700 dark:text-gray-400 justify-self-end">
-            <RiBuilding3Line size={20} />
-            <p className="text-xs">
-              {quote.source}
-            </p>
-          </div>
+      data-testid="quote-img"
+      className="flex rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800 flex-col hover:bg-gray-100 dark:hover:bg-gray-700"
+    >
+      <div className="h-full p-4 grid grid-cols-4  md:grid-cols-8 lg:grid-cols-12 gap-y-2 gap-x-3 lg:gap-x-2">
+        <div className="row-span-2 flex justify-center items-center cursor-pointer">
+          <Checkbox className="cursor-pointer" onChange={(event) => handleCheckboxChange(event)} />
         </div>
+        <div className="md:col-span-2 lg:col-span-3 row-span-2 place-self-center">
+          { isPaquetExpProvider && (<PaqueteExpressIcon />) }
+          { isFedexProvider && (
+            <picture className="flex h-18 lg:h-24 w-18 lg:w-24 bg-gray-800 rounded-full justify-center items-center">
+              <Image src={quote.logoSrc.source} alt="Fedex provider" width={quote.logoSrc.width} height={quote.logoSrc.height} />
+            </picture>
+          ) }
+          { (isOtherProvider || is99Provider) && (
+            <picture className="flex h-16 w-16 md:h-24 md:w-24 dark:bg-gray-100 rounded-full justify-center items-center">
+              <Image src={quote.logoSrc.source} alt="Other provider" width={quote.logoSrc.width} height={quote.logoSrc.height} />
+            </picture>
+          ) }
+          { (!isPaquetExpProvider && !isOtherProvider && !isFedexProvider && !is99Provider) && (
+            <Image src={quote.logoSrc.source} alt="Quote provider" width={quote.logoSrc.width} height={quote.logoSrc.height} />
+          )}
+        </div>
+        <h5 className={titleStyles}>
+          {quote.service}
+        </h5>
+        <p className="md:col-span-2 lg:col-span-3 md:row-span-2 font-semibold text-2xl">
+          {quote.amountFormatted}
+        </p>
+        <div className="md:col-span-2 inline-flex gap-2 text-gray-700 dark:text-gray-400 justify-self-end">
+          <RiBuilding3Line size={20} />
+          <p className="text-xs">
+            {quote.source}
+          </p>
+        </div>
+      </div>
     </article>
   )
 }
