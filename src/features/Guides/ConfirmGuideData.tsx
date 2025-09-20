f658d8994201129788f3872aa5be999c226d3f12
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-09-20 11:19:34 -0600

    feat: simplify markup in ConfirmGuideData for improved readability

--- src/features/Guides/ConfirmGuideData.tsx
+++ src/features/Guides/ConfirmGuideData.tsx
@@ -16,39 +16,39 @@ export const ConfirmGuideData = ({ formData, goPrev }: ConfirmGuideDataProps) =>
       <article className="flex flex-col gap-4">
         <h5 className="text-lg font-bold">Datos del remitente</h5>
         <ul className="grid grid-cols-1 lg:grid-cols-2 gap-2 list-disc">
-          <li className="ml-6"><span className="font-semibold">Nombre de la persona:</span> {originAddress.name}</li>
-          <li className="ml-6 text-base"><span className="font-semibold">Teléfono de contacto:</span> {formatPhoneNumber(originAddress.phone)}</li>
-          <li className="ml-6 text-base"><span className="font-semibold">Correo electrónico:</span> {originAddress.email}</li>
-          <li className="ml-6 text-base"><span className="font-semibold">Nombre de la compañia:</span> {originAddress.company}</li>
-          <li className="ml-6 text-base"><span className="font-semibold">Domicilio:</span> {originAddress.street1}</li>
-          <li className="ml-6 text-base"><span className="font-semibold">Colonia:</span> {originAddress.neighborhood}</li>
-          <li className="ml-6 text-base"><span className="font-semibold">Numero exterior:</span> {originAddress.external_number}</li>
-          <li className="ml-6 text-base"><span className="font-semibold">Ciudad:</span> {originAddress.city}</li>
-          <li className="ml-6 text-base"><span className="font-semibold">Estado:</span> {originAddress.state}</li>
-          <li className="ml-6 text-base"><span className="font-semibold">Referencia del domicilio:</span> {originAddress.reference}</li>
+          <li className="ml-6">Nombre de la persona: {originAddress.name}</li>
+          <li className="ml-6 text-base">Teléfono de contacto: {formatPhoneNumber(originAddress.phone)}</li>
+          <li className="ml-6 text-base">Correo electrónico: {originAddress.email}</li>
+          <li className="ml-6 text-base">Nombre de la compañia: {originAddress.company}</li>
+          <li className="ml-6 text-base">Domicilio: {originAddress.street1}</li>
+          <li className="ml-6 text-base">Colonia: {originAddress.neighborhood}</li>
+          <li className="ml-6 text-base">Numero exterior: {originAddress.external_number}</li>
+          <li className="ml-6 text-base">Ciudad: {originAddress.city}</li>
+          <li className="ml-6 text-base">Estado: {originAddress.state}</li>
+          <li className="ml-6 text-base">Referencia del domicilio: {originAddress.reference}</li>
         </ul>
       </article>
       <article className="flex flex-col gap-4">
         <h5 className="text-lg font-bold">Datos del destinatario</h5>
         <ul className="grid grid-cols-1 lg:grid-cols-2 gap-2 list-disc">
-          <li className="ml-6"><span className="font-semibold">Nombre de la persona:</span> {destinationAddress.name}</li>
-          <li className="ml-6 text-base"><span className="font-semibold">Teléfono de contacto:</span> {formatPhoneNumber(destinationAddress.phone)}</li>
-          <li className="ml-6 text-base"><span className="font-semibold">Correo electrónico:</span> {destinationAddress.email}</li>
-          <li className="ml-6 text-base"><span className="font-semibold">Nombre de la compañia:</span> {destinationAddress.company}</li>
-          <li className="ml-6 text-base"><span className="font-semibold">Domicilio:</span> {destinationAddress.street1}</li>
-          <li className="ml-6 text-base"><span className="font-semibold">Colonia:</span> {destinationAddress.neighborhood}</li>
-          <li className="ml-6 text-base"><span className="font-semibold">Numero exterior:</span> {destinationAddress.external_number}</li>
-          <li className="ml-6 text-base"><span className="font-semibold">Ciudad:</span> {destinationAddress.city}</li>
-          <li className="ml-6 text-base"><span className="font-semibold">Estado:</span> {destinationAddress.state}</li>
-          <li className="ml-6 text-base"><span className="font-semibold">Referencia del domicilio:</span> {destinationAddress.reference}</li>
+          <li className="ml-6">Nombre de la persona: {destinationAddress.name}</li>
+          <li className="ml-6 text-base">Teléfono de contacto: {formatPhoneNumber(destinationAddress.phone)}</li>
+          <li className="ml-6 text-base">Correo electrónico: {destinationAddress.email}</li>
+          <li className="ml-6 text-base">Nombre de la compañia: {destinationAddress.company}</li>
+          <li className="ml-6 text-base">Domicilio: {destinationAddress.street1}</li>
+          <li className="ml-6 text-base">Colonia: {destinationAddress.neighborhood}</li>
+          <li className="ml-6 text-base">Numero exterior: {destinationAddress.external_number}</li>
+          <li className="ml-6 text-base">Ciudad: {destinationAddress.city}</li>
+          <li className="ml-6 text-base">Estado: {destinationAddress.state}</li>
+          <li className="ml-6 text-base">Referencia del domicilio: {destinationAddress.reference}</li>
         </ul>
       </article>
       <article className="flex flex-col gap-4">
         <h5 className="text-lg font-bold">Información del paquete</h5>
         <ul className="grid grid-cols-1 gap-2 list-disc">
-          <li className="ml-6"><span className="font-semibold">Descripción del contenido del paquete:</span> {parcelInfo.content}</li>
-          <li className="ml-6"><span className="font-semibold">Valor del paquete:</span> {formatNumberToCurrency(parcelInfo.value)}</li>
-          <li className="ml-6"><span className="font-semibold">Cantidad:</span> {parcelInfo.quantity}</li>
+          <li className="ml-6">Descripción del contenido del paquete: {parcelInfo.content}</li>
+          <li className="ml-6">Valor del paquete: {formatNumberToCurrency(parcelInfo.value)}</li>
+          <li className="ml-6">Cantidad: {parcelInfo.quantity}</li>
         </ul>
       </article>
       <footer className="flex justify-between">
