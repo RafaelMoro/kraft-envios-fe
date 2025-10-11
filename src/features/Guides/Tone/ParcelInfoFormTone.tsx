f87a315db55ba2b1ee01888f9e9c3f0c7929602b
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-10-11 14:49:36 -0600

    feat: add ToggleSwitch for notification preference in ParcelInfoFormTone

--- src/features/Guides/Tone/ParcelInfoFormTone.tsx
+++ src/features/Guides/Tone/ParcelInfoFormTone.tsx
@@ -1,8 +1,9 @@
-import { Button, Label, TextInput } from "flowbite-react"
+import { Button, Label, TextInput, ToggleSwitch } from "flowbite-react"
 import { yupResolver } from "@hookform/resolvers/yup"
 import { SubmitHandler, useForm } from "react-hook-form"
 import { ParcelInfoFormValuesFormtoneSchema, ParcelInfoFormValuesTone } from "@/shared/types/guides.types"
 import { ErrorMessage } from "@/shared/ui/atoms/ErrorMessage"
+import { useState } from "react"
 
 interface CreateGuideFormValuesTone {
   isMobileTablet: boolean
@@ -13,6 +14,8 @@ interface CreateGuideFormValuesTone {
 }
 
 export const ParcelInfoFormTone = ({ isMobileTablet, parcelInfo, goNext, goPrev, updateParcelInfo }: CreateGuideFormValuesTone) => {
+  const [notifyMe, setNotifyMe] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -33,21 +36,24 @@ export const ParcelInfoFormTone = ({ isMobileTablet, parcelInfo, goNext, goPrev,
       onSubmit={handleSubmit(onSubmit)}
     >
       { isMobileTablet && (<h5 className="text-xl font-bold text-center mb-5">Información del paquete</h5>)}
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="content">Contenido del paquete</Label>
+      <section className="flex flex-col gap-4">
+        <div>
+          <div className="mb-2 block">
+            <Label htmlFor="content">Contenido del paquete</Label>
+          </div>
+          <TextInput
+            data-testid="content"
+            defaultValue={parcelInfo.content}
+            id="content"
+            type="text"
+            {...register("content")}
+          />
+          { errors?.content?.message && (
+            <ErrorMessage>{errors.content?.message}</ErrorMessage>
+          )}
         </div>
-        <TextInput
-          data-testid="content"
-          defaultValue={parcelInfo.content}
-          id="content"
-          type="text"
-          {...register("content")}
-        />
-        { errors?.content?.message && (
-          <ErrorMessage>{errors.content?.message}</ErrorMessage>
-        )}
-      </div>
+        <ToggleSwitch checked={notifyMe} label="Notificame" onChange={setNotifyMe} />
+      </section>
       <div className="flex justify-between mt-4">
         <Button
           color="light"
