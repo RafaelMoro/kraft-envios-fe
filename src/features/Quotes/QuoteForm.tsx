99115e4927a8be738b6adacd3d2cb64a3f2ab59c
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-10-12 14:50:26 -0600

    refactor: replace individual input fields with QuoteInput component for better reusability in QuoteForm

--- src/features/Quotes/QuoteForm.tsx
+++ src/features/Quotes/QuoteForm.tsx
@@ -1,15 +1,13 @@
 "use client"
+import { useEffect } from "react"
 import { yupResolver } from "@hookform/resolvers/yup"
 import { useMutation } from "@tanstack/react-query"
-import { Button, Label, Spinner, TextInput } from "flowbite-react"
+import { Button, Spinner } from "flowbite-react"
 import { SubmitHandler, useForm } from "react-hook-form"
 
 import { GeneralApiError } from "@/shared/types/global.types"
 import { GetQuoteDataAxios, GetQuoteForm, Quote, QuoteFormSchema } from "@/shared/types/quotes.types"
-import { ErrorMessage } from "@/shared/ui/atoms/ErrorMessage"
 import { getQuoteMutationCb } from "@/shared/utils/quotes.utils"
-import { useEffect } from "react"
-import { RiCloseLine } from "@remixicon/react"
 import { QuoteInput } from "./QuoteInput"
 
 interface QuoteFormProps {
@@ -72,112 +70,53 @@ export const QuoteForm = ({ updateQuotes, resetSelectedQuotes, resetFiltersQuote
             register={register}
             errorMessage={errors.originPostalCode?.message}
           />
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="destinationPostalCode">Código Postal de Destino</Label>
-            </div>
-            <div className="grid grid-cols-1 grid-rows-1">
-              <TextInput
-                id="destinationPostalCode"
-                className="col-start-1 col-end-2 row-start-1 row-end-2"
-                type="text"
-                inputMode="numeric"
-                {...register("destinationPostalCode")}
-              />
-              <button className="justify-self-end mr-2 col-start-1 col-end-2 row-start-1 row-end-2 z-10" onClick={() => clearInput("destinationPostalCode")}>
-                <RiCloseLine />
-              </button>
-            </div>
-            { errors.destinationPostalCode?.message && (
-              <ErrorMessage>{errors.destinationPostalCode?.message}</ErrorMessage>
-            )}
-          </div>
+          <QuoteInput
+            label="Código Postal de Destino"
+            inputId="destinationPostalCode"
+            inputType="text"
+            isNumericInput
+            clearInput={clearInput}
+            register={register}
+            errorMessage={errors.destinationPostalCode?.message}
+          />
         </div>
       </section>
 
       <section className="flex flex-col gap-5">
         <h4 className="text-xl font-semibold">Paquete:</h4>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="length">Largo</Label>
-            </div>
-            <div className="grid grid-cols-1 grid-rows-1">
-              <TextInput
-                id="length"
-                type="number"
-                className="col-start-1 col-end-2 row-start-1 row-end-2"
-                required
-                {...register("length")}
-              />
-              <button className="justify-self-end mr-6 col-start-1 col-end-2 row-start-1 row-end-2 z-10" onClick={() => clearInput("length")}>
-                <RiCloseLine />
-              </button>
-            </div>
-            { errors.length?.message && (
-              <ErrorMessage>{errors.length?.message}</ErrorMessage>
-            )}
-          </div>
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="height">Alto</Label>
-            </div>
-            <div className="grid grid-cols-1 grid-rows-1">
-              <TextInput
-                id="height"
-                className="col-start-1 col-end-2 row-start-1 row-end-2"
-                type="number"
-                required
-                {...register("height")}
-              />
-              <button className="justify-self-end mr-6 col-start-1 col-end-2 row-start-1 row-end-2 z-10" onClick={() => clearInput("height")}>
-                <RiCloseLine />
-              </button>
-            </div>
-            { errors.height?.message && (
-              <ErrorMessage>{errors.height?.message}</ErrorMessage>
-            )}
-          </div>
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="width">Ancho</Label>
-            </div>
-            <div className="grid grid-cols-1 grid-rows-1">
-              <TextInput
-                id="width"
-                className="col-start-1 col-end-2 row-start-1 row-end-2"
-                type="number"
-                required
-                {...register("width")}
-              />
-              <button className="justify-self-end mr-6 col-start-1 col-end-2 row-start-1 row-end-2 z-10" onClick={() => clearInput("width")}>
-                <RiCloseLine />
-              </button>
-            </div>
-            { errors.width?.message && (
-              <ErrorMessage>{errors.width?.message}</ErrorMessage>
-            )}
-          </div>
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="weight">Peso</Label>
-            </div>
-            <div className="grid grid-cols-1 grid-rows-1">
-              <TextInput
-                id="weight"
-                className="col-start-1 col-end-2 row-start-1 row-end-2"
-                type="number"
-                required
-                {...register("weight")}
-              />
-              <button className="justify-self-end mr-6 col-start-1 col-end-2 row-start-1 row-end-2 z-10" onClick={() => clearInput("weight")}>
-                <RiCloseLine />
-              </button>
-            </div>
-            { errors.weight?.message && (
-              <ErrorMessage>{errors.weight?.message}</ErrorMessage>
-            )}
-          </div>
+          <QuoteInput
+            label="Largo"
+            inputId="length"
+            inputType="number"
+            clearInput={clearInput}
+            register={register}
+            errorMessage={errors.length?.message}
+          />
+          <QuoteInput
+            label="Alto"
+            inputId="height"
+            inputType="number"
+            clearInput={clearInput}
+            register={register}
+            errorMessage={errors.height?.message}
+          />
+          <QuoteInput
+            label="Ancho"
+            inputId="width"
+            inputType="number"
+            clearInput={clearInput}
+            register={register}
+            errorMessage={errors.width?.message}
+          />
+          <QuoteInput
+            label="Peso"
+            inputId="weight"
+            inputType="number"
+            clearInput={clearInput}
+            register={register}
+            errorMessage={errors.weight?.message}
+          />
         </div>
       </section>
       <div className="flex justify-center gap-6">
