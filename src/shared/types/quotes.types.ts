f9416e8faac6ea8c33154f70f2fbd3b90049328c
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-10-13 12:27:50 -0600

    feat: enhance QuoteForm validation with transform for empty string handling

--- src/shared/types/quotes.types.ts
+++ src/shared/types/quotes.types.ts
@@ -111,8 +111,17 @@ export type GetQuoteForm = {
 export const QuoteFormSchema: ObjectSchema<GetQuoteForm> = object().shape({
   originPostalCode: string().required('La dirección postal de origen es requerida').min(5, 'La dirección postal de origen debe tener 5 caracteres').max(5, 'La dirección postal de origen debe tener 5 caracteres'),
   destinationPostalCode: string().required('La dirección postal de destino es requerida').min(5, 'La dirección postal de destino debe tener 5 caracteres').max(5, 'La dirección postal de destino debe tener 5 caracteres'),
-  weight: number().required('El peso del paquete es requerido').min(1, 'El peso debe ser mayor que 0'),
-  length: number().required('El largo del paquete es requerido').min(1, 'El largo debe ser mayor que 0'),
-  height: number().required('La altura del paquete es requerida').min(1, 'La altura debe ser mayor que 0'),
-  width: number().required('El ancho del paquete es requerido').min(1, 'El ancho debe ser mayor que 0'),
+  weight: number()
+    .transform((value, originalValue) => originalValue === '' ? undefined : value)
+    .required('El peso del paquete es requerido')
+    .min(1, 'El peso debe ser mayor que 0'),
+  length: number()
+    .transform((value, originalValue) => originalValue === '' ? undefined : value)
+    .required('El largo del paquete es requerido').min(1, 'El largo debe ser mayor que 0'),
+  height: number()
+    .transform((value, originalValue) => originalValue === '' ? undefined : value)
+    .required('La altura del paquete es requerida').min(1, 'La altura debe ser mayor que 0'),
+  width: number()
+    .transform((value, originalValue) => originalValue === '' ? undefined : value)
+    .required('El ancho del paquete es requerido').min(1, 'El ancho debe ser mayor que 0'),
 })
