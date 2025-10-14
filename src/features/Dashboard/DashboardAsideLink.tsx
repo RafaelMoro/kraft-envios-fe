f50cc611202de32f1e3eb454a07dc711ef743135
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-10-13 20:56:44 -0600

    fix: update button styles in DashboardAsideLink component

--- src/features/Dashboard/DashboardAsideLink.tsx
+++ src/features/Dashboard/DashboardAsideLink.tsx
@@ -9,8 +9,8 @@ interface DashboardAsideLinkProps {
 
 export const DashboardAsideLink = ({ onClickCb, children, isSelected }: DashboardAsideLinkProps) => {
   const buttonCss = clsx(
-    "text-gray-400 flex items-center gap-x-2.5 rounded-md px-2 py-1.5 text-sm transition hover:bg-indigo-900 dark:hover:bg-indigo-700 focus-visible:outline-2",
-    { "text-white dark:text-indigo-400": isSelected }
+    "text-gray-300 flex items-center gap-x-2.5 rounded-md px-2 py-1.5 text-sm transition hover:bg-indigo-900 dark:hover:bg-indigo-700 focus-visible:outline-2",
+    { "text-white bg-indigo-900 dark:bg-indigo-700": isSelected }
   )
 
   return (
