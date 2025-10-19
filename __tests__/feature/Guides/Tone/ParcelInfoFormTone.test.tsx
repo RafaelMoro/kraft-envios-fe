03bf88f9e5f6991edb44e64bfeb5c0631af7e5fb
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-10-19 12:09:26 -0600

    feat: refactor ParcelInfoFormTone import path and create ParcelInfo component for form handling

--- __tests__/feature/Guides/Tone/ParcelInfoFormTone.test.tsx
+++ __tests__/feature/Guides/Tone/ParcelInfoFormTone.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 
-import { ParcelInfoFormTone } from '@/features/Guides/Tone/ParcelInfo'
+import { ParcelInfoFormTone } from '@/features/Guides/ParcelInfo'
 import { ParcelInfoFormValuesTone } from '@/shared/types/guides.types'
 
 describe('ParcelInfoFormTone', () => {
