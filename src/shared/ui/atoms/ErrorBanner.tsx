de2687760ebac2cade166009e61e386da3695a9e
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-10-26 08:56:51 -0600

    refactor: update ErrorBanner layout for improved styling and structure

--- src/shared/ui/atoms/ErrorBanner.tsx
+++ src/shared/ui/atoms/ErrorBanner.tsx
@@ -7,14 +7,14 @@ interface ErrorBannerProps {
 
 export const ErrorBanner = ({ message, toggleError }: ErrorBannerProps) => {
   return (
-    <div className="grid grid-rows-2 gap-5 justify-items-center">
-      <button className="justify-self-end" onClick={toggleError}>
-        <RiCloseLine />
-      </button>
+    <div className="flex justify-between p-2 border border-red-500 bg-red-100 rounded-full">
       <div className="inline-flex gap-2 text-red-500">
         <RiErrorWarningLine />
         <p>{message}</p>
       </div>
+      <button className="justify-self-end text-red-500" onClick={toggleError}>
+        <RiCloseLine />
+      </button>
     </div>
   )
 }
