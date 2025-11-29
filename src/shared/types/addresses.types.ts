dc98c5275ac98421a84e2eb97b05cdd399077d16
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-11-29 10:47:41 -0600

    feat: add CreateAddressFormSchema for address validation

---
+++ src/shared/types/addresses.types.ts
@@ -0,0 +1,54 @@
+import { object, ObjectSchema, string } from "yup";
+
+const onlyNumberRegex = /^\d+$/;
+
+export type CreateAddressFormValues = {
+  street1: string;
+  externalNumber: string;
+  internalNumber?: string | null | undefined;
+  neighborhood: string;
+  zipcode: string;
+  city: string;
+  town: string;
+  state: string;
+  reference?: string | null | undefined;
+  alias: string;
+}
+
+export const CreateAddressFormSchema: ObjectSchema<CreateAddressFormValues> = object().shape({
+  street1: string().required('Nombre es requerido').min(2, 'El nombre debe tener al menos 2 caracteres'),
+  externalNumber: string()
+    .required('El número exterior es requerido')
+    .matches(onlyNumberRegex, { excludeEmptyString: true, message: "El número exterior solo puede contener dígitos" })
+    .min(1, 'El número exterior debe ser al menos de 1 dígito'),
+  internalNumber: string()
+    .nullable()
+    .notRequired()
+    .when('internalNumber', {
+      is: (value: string) => value?.length,
+      then: (rule) => rule
+        .matches(onlyNumberRegex, { excludeEmptyString: true, message: "El número interior solo puede contener dígitos" })
+        .min(1, 'El número interior debe ser al menos de 1 dígito'),
+    }),
+  neighborhood: string().required('Colonia es requerida').min(2, 'La colonia debe tener al menos 2 caracteres'),
+  zipcode: string()
+      .required('El código postal es requerido')
+      .matches(onlyNumberRegex, { excludeEmptyString: true, message: "El código postal solo puede contener dígitos" })
+      .min(5, 'El código postal debe tener 5 caracteres')
+      .max(5, 'El código postal debe tener 5 caracteres'),
+  city: string().required('Ciudad es requerida').min(2, 'La ciudad debe tener al menos 2 caracteres'),
+  town: string().required('Municipio es requerido').min(2, 'El municipio debe tener al menos 2 caracteres'),
+  state: string().required('Estado es requerido').min(2, 'El estado debe tener al menos 2 caracteres'),
+  reference: string()
+    .nullable()
+    .notRequired()
+    .when('reference', {
+      is: (value: string) => value?.length,
+      then: (rule) => rule
+        .min(1, 'La referencia debe tener al menos 1 carácter'),
+    }),
+  alias: string().required('Alias es requerido').min(2, 'El alias debe tener al menos 2 caracteres'),
+}, [
+  ["internalNumber", "internalNumber"],
+  ["reference", "reference"]
+])
