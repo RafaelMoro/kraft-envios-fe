862692fde23ff188831906c36d79b66cd24e07f6
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-11-30 08:38:35 -0600

    feat: enhance AddressCard layout and improve city/town display

--- src/features/Addresses/AddressCard.tsx
+++ src/features/Addresses/AddressCard.tsx
@@ -1,5 +1,5 @@
 import { Badge, Button, Card } from "flowbite-react"
-import { RiBuildingLine, RiDeleteBinLine, RiMap2Line, RiMapPin2Line, RiPencilLine } from "@remixicon/react"
+import { RiDeleteBinLine, RiMapPin2Line, RiPencilLine } from "@remixicon/react"
 
 import { Address } from "@/shared/types/addresses.types"
 
@@ -10,7 +10,7 @@ interface AddressCardProps {
 export const AddressCard = ({ address }: AddressCardProps) => {
   return (
     <Card className="max-w-sm">
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-3">
         <div className="inline-flex gap-2">
           <RiMapPin2Line />
           <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -41,19 +41,21 @@ export const AddressCard = ({ address }: AddressCardProps) => {
                 </p>
             </div>
             )}
-            <div className="flex gap-3 flex-wrap">
-              <RiBuildingLine size={18} />
-              <span className="text-sm">Ciudades:</span>
-              { address.city.map((city) => (
-                <Badge color="info" size="xs" key={city}>{city}</Badge>
-              ))}
+            <div className="flex flex-col gap-1">
+              <p className="text-gray-500 dark:text-gray-400">Ciudades</p>
+              <div className="flex gap-3 flex-wrap">
+                { address.city.map((city) => (
+                  <Badge color="info" size="xs" key={city}>{city}</Badge>
+                ))}
+              </div>
             </div>
-            <div className="flex gap-3 flex-wrap">
-              <RiMap2Line size={18} />
-              <span className="text-sm">Municipios:</span>
-              { address.town.map((town) => (
-                <Badge color="success" size="xs" key={town}>{town}</Badge>
-              ))}
+            <div className="flex flex-col gap-1">
+              <p className="text-gray-500 dark:text-gray-400">Municipios</p>
+              <div className="flex gap-3 flex-wrap">
+                { address.town.map((town) => (
+                  <Badge color="success" size="xs" key={town}>{town}</Badge>
+                ))}
+              </div>
             </div>
           </>
         )}
@@ -62,7 +64,7 @@ export const AddressCard = ({ address }: AddressCardProps) => {
             <RiPencilLine size={18} />
             Editar
           </Button>
-          <Button className="border-0 inline-flex gap-2" outline>
+          <Button className="border-0 inline-flex gap-2 text-red-700" outline>
             <RiDeleteBinLine size={18} />
             Eliminar
           </Button>
