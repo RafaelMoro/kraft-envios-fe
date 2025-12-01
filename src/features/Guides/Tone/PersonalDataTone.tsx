741792e5bbf8d7c32874babcbb06afd6f536d16f
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-12-01 08:09:46 -0600

    feat: enhance PersonalDataTone component with props for address data and error handling

--- src/features/Guides/Tone/PersonalDataTone.tsx
+++ src/features/Guides/Tone/PersonalDataTone.tsx
@@ -1,24 +1,33 @@
+import { CreateGuideAddressFormValuesTone } from "@/shared/types/guides.types"
+import { ErrorMessage } from "@/shared/ui/atoms/ErrorMessage";
 import { Label, TextInput } from "flowbite-react"
+import { FieldErrors, UseFormRegister } from "react-hook-form"
 
-export const PersonalDataTone = () => {
+interface PersonalDataToneProps {
+  addressData: CreateGuideAddressFormValuesTone;
+  errors: FieldErrors<CreateGuideAddressFormValuesTone>;
+  register: UseFormRegister<CreateGuideAddressFormValuesTone>
+}
+
+export const PersonalDataTone = ({ addressData, errors, register }: PersonalDataToneProps) => {
   return (
-    <>
+    <section>
       <h4 className="text-xl">Datos personales</h4>
-      <section className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         <div>
           <div className="mb-2 block">
             <Label htmlFor="name">Nombre</Label>
           </div>
           <TextInput
             data-testid="name"
-            // defaultValue={addressData.name}
+            defaultValue={addressData.name}
             id="name"
             type="text"
-            // {...register("name")}
+            {...register("name")}
           />
-          {/* { errors?.name?.message && (
+          { errors?.name?.message && (
             <ErrorMessage>{errors.name?.message}</ErrorMessage>
-          )} */}
+          )}
         </div>
         <div>
           <div className="mb-2 block">
@@ -26,14 +35,14 @@ export const PersonalDataTone = () => {
           </div>
           <TextInput
             data-testid="lastName"
-            // defaultValue={addressData.lastName}
+            defaultValue={addressData.lastName}
             id="lastName"
             type="text"
-            // {...register("lastName")}
+            {...register("lastName")}
           />
-          {/* { errors?.lastName?.message && (
+          { errors?.lastName?.message && (
             <ErrorMessage>{errors.lastName?.message}</ErrorMessage>
-          )} */}
+          )}
         </div>
         <div>
           <div className="mb-2 block">
@@ -44,12 +53,12 @@ export const PersonalDataTone = () => {
             id="phone"
             type="text"
             inputMode="numeric"
-            // defaultValue={addressData.phone}
-            // {...register("phone")}
+            defaultValue={addressData.phone}
+            {...register("phone")}
           />
-          {/* { errors?.phone?.message && (
+          { errors?.phone?.message && (
             <ErrorMessage>{errors?.phone?.message}</ErrorMessage>
-          )} */}
+          )}
         </div>
         <div>
           <div className="mb-2 block">
@@ -58,14 +67,14 @@ export const PersonalDataTone = () => {
           <TextInput
             id="email"
             type="email"
-            // defaultValue={addressData.email ?? ''}
-            // {...register("email")}
+            defaultValue={addressData.email ?? ''}
+            {...register("email")}
           />
-          {/* { errors?.email?.message && (
+          { errors?.email?.message && (
             <ErrorMessage>{errors.email?.message}</ErrorMessage>
-          )} */}
+          )}
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   )
 }
