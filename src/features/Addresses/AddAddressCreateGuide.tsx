04e3243f176d4bc1509fb9578265cd9987cfed19
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-12-07 09:10:31 -0600

    feat: adjust spacing and styling in AddAddressCreateGuide for improved layout

--- src/features/Addresses/AddAddressCreateGuide.tsx
+++ src/features/Addresses/AddAddressCreateGuide.tsx
@@ -11,9 +11,9 @@ export const AddAddressCreateGuide = ({ PersonalDataUI, CreateTempAddressButton,
   return (
     <div>
       {PersonalDataUI}
-      <div className="flex flex-col gap-6 mt-8">
+      <div className="flex flex-col gap-4 mt-8">
         <h4 className="text-xl">Domicilio</h4>
-        <p>Selecciona una dirección o llene una dirección temporal</p>
+        <p className="text-sm text-gray-600 dark:text-gray-400">Selecciona una dirección o llene una dirección temporal</p>
         {children}
       </div>
       {CreateTempAddressButton}
