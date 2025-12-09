2275648bdf9d2257620343477f1b27b4faaa8e7e
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-12-09 10:35:45 -0600

    feat: add AddAddressMn component for managing address input in guides

---
+++ src/features/Guides/Mn/AddAddressMn.tsx
@@ -0,0 +1,56 @@
+"use client"
+import { useState } from "react";
+
+import { useSelectAlias } from "@/shared/hooks/useAlias";
+import { AddTempAddressMn } from "./AddTempAddressMn";
+import { CreateGuideAddressFormValues } from "@/shared/types/guides.types";
+
+interface AddAddressMnProps {
+  title: string
+  addressData: CreateGuideAddressFormValues
+  isMobileTablet: boolean
+  isDestination?: boolean
+  goNext: () => void
+  goPrev: () => void
+  toggleModal: () => void
+  updateAddress: (data: CreateGuideAddressFormValues) => void
+}
+
+export const AddAddressMn = ({
+  isDestination, title, addressData, isMobileTablet, goNext, goPrev, toggleModal, updateAddress
+}: AddAddressMnProps) => {
+  const [useTempAddress, setUseTempAddress] = useState(false);
+  const toggleTempAddress = () => setUseTempAddress((prev) => !prev);
+
+  // TODO: Change alias saved
+  const {
+    aliasSelected, setAliasSelected, addressError, setAddressError, townError, cityError, setTownError, setCityError, resetAliasSelected
+  } = useSelectAlias({ aliasSaved: '' });
+
+  const cancelColorButton = isDestination ? "light" : "red"
+  const cancelButtonText = isDestination ? "Regresar" : "Cancelar"
+
+  const handleCancel = () => {
+    resetAliasSelected();
+    if (isDestination) {
+      goPrev()
+      return
+    }
+
+    toggleModal()
+  }
+
+  if (useTempAddress) {
+    return (
+      <AddTempAddressMn
+        title={title}
+        goNext={goNext}
+        updateAddress={updateAddress}
+        addressData={addressData}
+        isMobileTablet={isMobileTablet}
+        toggleModal={toggleTempAddress}
+        goPrev={goPrev}
+      />
+    )
+  }
+}
