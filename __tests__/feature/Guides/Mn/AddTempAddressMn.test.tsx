3807e7348899e0d57fc08d74ceb8d6b2290e4b3d
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-12-09 14:42:28 -0600

    feat: add tests for AddTempAddressMn component to validate form rendering and submission

--- __tests__/feature/Guides/Mn/AddTempAddressMn.test.tsx
+++ __tests__/feature/Guides/Mn/AddTempAddressMn.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { CreateGuideAddressForm } from '@/features/Guides/Mn/AddTempAddressMn'
+import { AddTempAddressMn } from '@/features/Guides/Mn/AddTempAddressMn'
 import { initialStateAddressForm } from '@/shared/constants/guides.constants'
 
 // Mock functions for props
@@ -22,7 +22,7 @@ const defaultProps = {
 
 const renderComponent = (props = {}) => {
   const mergedProps = { ...defaultProps, ...props }
-  return render(<CreateGuideAddressForm {...mergedProps} />)
+  return render(<AddTempAddressMn {...mergedProps} />)
 }
 
 describe('CreateGuideAddressForm', () => {
