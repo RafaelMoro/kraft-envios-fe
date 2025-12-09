ef5993270eb53057b33c7a4f8e4e30d3115aad84
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-12-09 11:31:51 -0600

    feat: create CreateGuideModalMn component for managing guide creation in Mn context

--- __tests__/feature/Guides/CreateGuideModal.test.tsx
+++ __tests__/feature/Guides/CreateGuideModal.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { CreateGuideModal } from '@/features/Guides/Mn/CreateGuideModal'
+import { CreateGuideModal } from '@/features/Guides/Mn/CreateGuideModalMn'
 import { QuoteUI } from '@/shared/types/quotes.types'
 import { mockMatchMedia, QueryMatchMedia } from '../../utils-test/mockWatchMedia'
 
