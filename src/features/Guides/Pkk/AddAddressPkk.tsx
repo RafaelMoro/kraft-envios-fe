e7263538d9fadc045a95e4c2eee9ede766306344
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-12-10 11:25:44 -0600

    feat: refactor AddAddressPkk component to include props and address handling logic

--- src/features/Guides/Pkk/AddAddressPkk.tsx
+++ src/features/Guides/Pkk/AddAddressPkk.tsx
@@ -1,3 +1,56 @@
-export const AddAddressPkk = () => {
-  
+import { useAddAddress } from "@/shared/hooks/useAddAddress";
+import { AliasSaved, CreateGuideAddressValuesPkk } from "@/shared/types/guides.types";
+import { AddTempAddressPkk } from "./AddTempAddressPkk";
+
+interface AddAddressPkkProps {
+  isDestination?: boolean
+  addressData: CreateGuideAddressValuesPkk;
+  // TODO: Change this to the correct type for Pkk
+  aliasSaved: AliasSaved
+  goNext: () => void
+  goPrev: () => void
+  toggleModal: () => void
+  updateAddress: (data: CreateGuideAddressValuesPkk) => void
+}
+
+export const AddAddressPkk = ({
+  isDestination = false, addressData, aliasSaved, goPrev, goNext, toggleModal, updateAddress
+}: AddAddressPkkProps) => {
+  const {
+    aliasSelected,
+    setAliasSelected,
+    addressError,
+    setAddressError,
+    townError,
+    cityError,
+    setTownError,
+    setCityError,
+    handleCancel,
+    addressType,
+    cancelButtonText,
+    cancelColorButton,
+    useTempAddress,
+    toggleTempAddress
+  } = useAddAddress({ isDestination, alias: aliasSaved.alias, toggleModal, goPrev });
+
+  if (useTempAddress) {
+    return (
+      <AddTempAddressPkk
+        isDestination={isDestination}
+        addressData={addressData}
+        goNext={goNext}
+        goPrev={goPrev}
+        updateOriginAddress={updateAddress}
+        toggleModal={toggleTempAddress}
+      />
+    )
+  }
+
+  return (
+    <form
+      // onSubmit={handleSubmit(onSubmit)}
+    >
+
+    </form>
+  )
 }
