dcc6869c37f171a55452ba65862d66fdf0322733
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-01-07 12:06:18 -0600

    feat: add ResultCreateAddress component for displaying address creation results

---
+++ src/features/Addresses/ResultCreateAddress.tsx
@@ -0,0 +1,28 @@
+import { RiCheckboxCircleFill } from "@remixicon/react";
+import { Button } from "flowbite-react";
+
+interface ResultCreateAddressProps {
+  isError?: boolean;
+}
+
+export const ResultCreateAddress = ({ isError }: ResultCreateAddressProps) => {
+  const title = isError
+    ? '¡Ups! Ocurrió un problema al añadir la dirección.'
+    : '¡Perfecto! Dirección añadida correctamente.'
+  return (
+    <section className="flex flex-col gap-10">
+      <h4 className="text-xl font-semibold text-center text-blue-800 dark:text-blue-900">{title}</h4>
+      <ul className="flex flex-col gap-3 items-center">
+        <li className="inline-flex gap-1">
+          <RiCheckboxCircleFill className="text-blue-800 dark:text-blue-900" />
+          Dirección creada en el sistema
+        </li>
+        <li className="inline-flex gap-1">
+          <RiCheckboxCircleFill className="text-blue-800 dark:text-blue-900" />
+          Dirección creada en GE
+        </li>
+      </ul>
+      <Button outline>Listo</Button>
+    </section>
+  )
+}
