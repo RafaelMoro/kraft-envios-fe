ecd2ebfff919117203f32213f9f2091b97e6a944
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-01-08 08:48:43 -0600

    feat: restructure PendingAddressGE layout for improved address display

--- src/features/Addresses/PendingAddressGE.tsx
+++ src/features/Addresses/PendingAddressGE.tsx
@@ -9,12 +9,17 @@ interface PendingAddressGEProps {
 export const PendingAddressGE = ({ address }: PendingAddressGEProps) => {
   return (
     <Card className="max-w-md">
-      <div className="flex justify-between">
+      <div className="grid grid-cols-2 gap-y-2">
         <div className="inline-flex gap-2">
           <RiMapPinFill />
           <h4 className="text-lg">{address.alias}</h4>
         </div>
-        <Button outline>Volver a intentar</Button>
+        <p className="text-sm text-gray-400 row-start-2 row-end-3">
+          {address.street} {address.number}, {address.neighborhood}, {address.city} {address.state}, C.P. {address.zipcode}
+        </p>
+        <div className="row-span-2 place-self-center">
+          <Button outline>Volver a intentar</Button>
+        </div>
       </div>
     </Card>
   )
