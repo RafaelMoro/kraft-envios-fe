7d8d71d7f5d3c2f4a4f37ba5b6173d2e0cd88ef4
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-01-09 12:36:23 -0600

    feat: add SelectOnlyAddressDropdown component for alias selection

---
+++ src/features/Addresses/SelectOnlyAddressDropdown.tsx
@@ -0,0 +1,48 @@
+import { Button, Dropdown, DropdownItem, Label, Spinner } from "flowbite-react"
+import { RiArrowDownSLine } from "@remixicon/react"
+
+import { useGetAddress } from "@/shared/hooks/useGetAddress"
+import { ErrorMessage } from "@/shared/ui/atoms/ErrorMessage"
+import { CreateGuideAddressValuesGE } from "@/shared/types/guides.types";
+
+interface SelectOnlyAddressDropdownProps {
+  addressData: CreateGuideAddressValuesGE;
+  errorMessage: string;
+  handleSelectAlias: (newAlias: string) => void;
+}
+
+export const SelectOnlyAddressDropdown = ({ addressData, errorMessage, handleSelectAlias }: SelectOnlyAddressDropdownProps) => {
+  const { data: addresses, isPending, isError } = useGetAddress()
+
+  return (
+    <Dropdown
+      label=""
+      renderTrigger={() => (
+        <div className="flex flex-col gap-1">
+          <Label className="pl-1">Alias</Label>
+          <Button
+            className="hover:cursor-pointer flex justify-between"
+            data-testid="select-address-dropdown-button"
+            color="light"
+            disabled={isPending || isError || !addresses || addresses.length === 0}
+          >
+            { isPending && (<Spinner />)}
+            { (isError && !isPending) && ("No se han podido cargar los alias")}
+            { (!isPending && addresses && addresses.length > 0 && !addressData.alias) && 'Alias de dirección'}
+            { (!isPending && addresses && addresses.length > 0 && addressData.alias) && addressData.alias}
+            <RiArrowDownSLine />
+          </Button>
+          { errorMessage && (
+            <ErrorMessage>{errorMessage}</ErrorMessage>
+          )}
+        </div>
+      )}
+    >
+      { (addresses && addresses.length > 0 )&& addresses.map((address) => (
+        <DropdownItem key={`alias-${address.alias}`} onClick={() => handleSelectAlias(address.alias)}>
+          {address.alias}
+        </DropdownItem>
+      )) }
+    </Dropdown>
+  )
+}
