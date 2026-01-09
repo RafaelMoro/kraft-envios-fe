504112400014ff94ed741be0bcb09b38b68629ff
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-01-09 12:52:06 -0600

    feat: create AddAddressGE component for address selection

---
+++ src/features/Guides/GE/AddAddressGE.tsx
@@ -0,0 +1,8 @@
+export const AddAddressGE = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <h4 className="text-xl">Domicilio</h4>
+      <p className="text-sm text-gray-600 dark:text-gray-400">Selecciona una dirección.</p>
+    </div>
+  )
+}
