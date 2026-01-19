be9a4e72d62eb9301f386d584706dfb5587857f9
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-01-19 12:43:51 -0600

    feat: enhance AddressRegionFields component with error handling and improved props

--- src/features/Addresses/AddressRegionFields.tsx
+++ src/features/Addresses/AddressRegionFields.tsx
@@ -1,17 +1,39 @@
 import { ReactNode } from "react";
 import { Label, TextInput } from "flowbite-react";
+import {
+  FieldError,
+  FieldErrors,
+  Path,
+  UseFormRegister,
+} from "react-hook-form";
+import { ErrorMessage } from "@/shared/ui/atoms/ErrorMessage";
 
-interface AddressRegionFieldsProps {
+type AddressRegionDataT = {
+  zipcode: string;
+  neighborhood: string;
+  state: string;
+};
+interface AddressRegionFieldsProps<T extends AddressRegionDataT> {
   CityField: ReactNode;
+  addressData: T;
+  errors: FieldErrors<T>;
+  register: UseFormRegister<T>;
 }
 
 /**
  * This component contains the region fields: state, city and neighborhood
  * It's the counterpart of the component `AutocompleteZipcode` that helps the user to fill these fields
  */
-export const AddressRegionFields = ({
+export const AddressRegionFields = <T extends AddressRegionDataT>({
   CityField,
-}: AddressRegionFieldsProps) => {
+  addressData,
+  errors,
+  register,
+}: AddressRegionFieldsProps<T>) => {
+  const zipcodeError = errors?.zipcode as FieldError | undefined;
+  const neighborhoodError = errors?.neighborhood as FieldError | undefined;
+  const stateError = errors?.state as FieldError | undefined;
+
   return (
     <>
       <div>
@@ -20,15 +42,15 @@ export const AddressRegionFields = ({
         </div>
         <TextInput
           data-testid="zipcode"
-          // defaultValue={formData.zipcode}
+          defaultValue={addressData.zipcode}
           id="zipcode"
           type="text"
           inputMode="numeric"
-          // {...register("zipcode")}
+          {...register("zipcode" as Path<T>)}
         />
-        {/* {errors?.zipcode?.message && (
-          <ErrorMessage>{errors.zipcode?.message}</ErrorMessage>
-        )} */}
+        {zipcodeError?.message && (
+          <ErrorMessage>{zipcodeError?.message}</ErrorMessage>
+        )}
       </div>
       <div>
         <div className="mb-2 block">
@@ -37,13 +59,13 @@ export const AddressRegionFields = ({
         <TextInput
           data-testid="neighborhood"
           id="neighborhood"
-          // defaultValue={formData.neighborhood}
+          defaultValue={addressData.neighborhood}
           type="text"
-          // {...register("neighborhood")}
+          {...register("neighborhood" as Path<T>)}
         />
-        {/* {errors?.neighborhood?.message && (
-          <ErrorMessage>{errors.neighborhood?.message}</ErrorMessage>
-        )} */}
+        {neighborhoodError?.message && (
+          <ErrorMessage>{neighborhoodError?.message}</ErrorMessage>
+        )}
       </div>
       <div>
         <div className="mb-2 block">
@@ -52,13 +74,13 @@ export const AddressRegionFields = ({
         <TextInput
           data-testid="state"
           id="state"
-          // defaultValue={formData.state}
+          defaultValue={addressData.state}
           type="text"
-          // {...register("state")}
+          {...register("state" as Path<T>)}
         />
-        {/* {errors?.state?.message && (
-          <ErrorMessage>{errors.state?.message}</ErrorMessage>
-        )} */}
+        {stateError?.message && (
+          <ErrorMessage>{stateError?.message}</ErrorMessage>
+        )}
       </div>
       {CityField}
     </>
