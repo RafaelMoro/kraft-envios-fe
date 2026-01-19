8a153d3f0af55c9ac0eb4d99a3affd0e5f661826
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-01-19 12:15:19 -0600

    feat: integrate AutocompleteZipcode component and improve code formatting in CreateAddressSubform

--- src/features/Addresses/CreateAddressSubform.tsx
+++ src/features/Addresses/CreateAddressSubform.tsx
@@ -1,25 +1,47 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
-import { Button, Checkbox, CheckIcon, Label, Spinner, TextInput, ToggleSwitch } from "flowbite-react"
-import { FieldErrors, SubmitHandler, UseFormHandleSubmit, UseFormRegister, UseFormSetError } from "react-hook-form";
+import {
+  Button,
+  Checkbox,
+  CheckIcon,
+  Label,
+  Spinner,
+  TextInput,
+  ToggleSwitch,
+} from "flowbite-react";
+import {
+  FieldErrors,
+  SubmitHandler,
+  UseFormHandleSubmit,
+  UseFormRegister,
+  UseFormSetError,
+} from "react-hook-form";
 
-import { ErrorMessage } from "@/shared/ui/atoms/ErrorMessage"
-import { AddTag } from "@/shared/ui/organisms/AddTag"
-import { CreateAddressFormValues, CreateAddressPayload, ManageAddressFormScreen } from "@/shared/types/addresses.types";
+import { ErrorMessage } from "@/shared/ui/atoms/ErrorMessage";
+import { AddTag } from "@/shared/ui/organisms/AddTag";
+import {
+  CreateAddressFormValues,
+  CreateAddressPayload,
+  ManageAddressFormScreen,
+} from "@/shared/types/addresses.types";
 import { useAddTag } from "@/shared/hooks/useAddTag";
 import { formatPayloadCreateAddress } from "@/shared/utils/addresses.utils";
 import { convertToAddressDataGEFormValues } from "@/shared/utils/guides.utils";
 import { AddressDataGEFormValues } from "@/shared/types/guides.types";
 import { ErrorBanner } from "@/shared/ui/atoms/ErrorBanner";
+import { AutocompleteZipcode } from "./AutocompleteZipcode";
 
 interface CreateAddressSubformProps {
   formData: CreateAddressPayload;
   isEdit: boolean;
-  actionText: "Editar" | "Crear"
-  errors: FieldErrors<CreateAddressFormValues>
-  register: UseFormRegister<CreateAddressFormValues>
-  handleSubmit: UseFormHandleSubmit<CreateAddressFormValues, CreateAddressFormValues>
-  setError: UseFormSetError<CreateAddressFormValues>
+  actionText: "Editar" | "Crear";
+  errors: FieldErrors<CreateAddressFormValues>;
+  register: UseFormRegister<CreateAddressFormValues>;
+  handleSubmit: UseFormHandleSubmit<
+    CreateAddressFormValues,
+    CreateAddressFormValues
+  >;
+  setError: UseFormSetError<CreateAddressFormValues>;
   createAddressMutation: (payload: CreateAddressPayload) => void;
   editAddressMutation: (payload: CreateAddressPayload) => void;
   isPending: boolean;
@@ -27,13 +49,13 @@ interface CreateAddressSubformProps {
   isPendingEdit: boolean;
   isSuccessEdit: boolean;
   toggleModal: () => void;
-  setSubscreen: (subscreen: ManageAddressFormScreen) => void
-  updateAddressDataGE: (data: AddressDataGEFormValues) => void
+  setSubscreen: (subscreen: ManageAddressFormScreen) => void;
+  updateAddressDataGE: (data: AddressDataGEFormValues) => void;
   hasConsentedOnce: boolean;
   setHasConsentedOnce: (consented: boolean) => void;
-  dataAliases: string[] | undefined
-  isPendingFetchAlias: boolean
-  errorAlias: Error | null
+  dataAliases: string[] | undefined;
+  isPendingFetchAlias: boolean;
+  errorAlias: Error | null;
 }
 
 export const CreateAddressSubform = ({
@@ -61,7 +83,7 @@ export const CreateAddressSubform = ({
 }: CreateAddressSubformProps) => {
   // Create address in GE states
   const [shouldCreateGEAddress, setShouldCreateGEAddress] = useState(false);
-  const [consentSkipGECreation , setConsentSkipGECreation] = useState(false);
+  const [consentSkipGECreation, setConsentSkipGECreation] = useState(false);
   const [showConsentError, setShowConsentError] = useState(false);
 
   const handleShouldCreateGEAddress = () => {
@@ -72,21 +94,21 @@ export const CreateAddressSubform = ({
       }
       return nextValue;
     });
-    
+
     // Set that the user has consented at least once (outside the state updater)
     if (!shouldCreateGEAddress && !hasConsentedOnce) {
       setHasConsentedOnce(true);
     }
-  }
+  };
 
   const handleConsentSkipGECreation = (isChecked: boolean) => {
     setConsentSkipGECreation(isChecked);
     if (showConsentError) setShowConsentError(false);
-  }
+  };
 
   const [errorFetchAlias, setErrorFetchAlias] = useState<string | null>(null);
   const [showErrorBanner, setShowErrorBanner] = useState<boolean>(false);
-  const toggleErrorBanner = () => setShowErrorBanner((prev) => !prev)
+  const toggleErrorBanner = () => setShowErrorBanner((prev) => !prev);
 
   const {
     tags: towns,
@@ -94,92 +116,108 @@ export const CreateAddressSubform = ({
     removeTag: removeTown,
     validateTagsEmpty: validateTownsEmpty,
     error: townsError,
-    setError: setTownsError
-  } = useAddTag({ tagsInitState: (formData?.town ?? []) })
+    setError: setTownsError,
+  } = useAddTag({ tagsInitState: formData?.town ?? [] });
   const {
     tags: cities,
     addTag: addCity,
     removeTag: removeCity,
     validateTagsEmpty: validateCitiesEmpty,
     error: citiesError,
-    setError: setCitiesError
-  } = useAddTag({ tagsInitState: (formData?.city ?? []) })
+    setError: setCitiesError,
+  } = useAddTag({ tagsInitState: formData?.city ?? [] });
 
-  const submitButtonText = shouldCreateGEAddress ? 'Siguiente' : `${actionText} dirección`;
+  const submitButtonText = shouldCreateGEAddress
+    ? "Siguiente"
+    : `${actionText} dirección`;
 
   // Reset error if data is present
   useEffect(() => {
     if (Boolean(dataAliases) && errorFetchAlias) {
-      setErrorFetchAlias(null)
-      setShowErrorBanner(false)
+      setErrorFetchAlias(null);
+      setShowErrorBanner(false);
     }
-  }, [dataAliases, errorFetchAlias])
+  }, [dataAliases, errorFetchAlias]);
 
   // Log error fetching alias addresses
   useEffect(() => {
     if (errorAlias) {
-      console.error('Error fetching alias addresses from GE:', errorAlias)
-      setErrorFetchAlias('Ocurrió un error al obtener los alias. Por favor, intenta de nuevo más tarde.')
-      setShowErrorBanner(true)
+      console.error("Error fetching alias addresses from GE:", errorAlias);
+      setErrorFetchAlias(
+        "Ocurrió un error al obtener los alias. Por favor, intenta de nuevo más tarde.",
+      );
+      setShowErrorBanner(true);
     }
-  }, [errorAlias])
+  }, [errorAlias]);
 
   const onSubmit: SubmitHandler<CreateAddressFormValues> = (data, event) => {
-      event?.preventDefault()
-      if (showConsentError) setShowConsentError(false)
-  
-      // Check if alias has been modified in edit mode
-      if (isEdit && formData?.alias && data?.alias !== formData.alias) {
-        setError('alias', {
-          type: 'manual',
-          message: 'El alias no puede ser editado'
-        })
-        return
-      }
-  
-      const townsEmpty = validateTownsEmpty()
-      const citiesEmpty = validateCitiesEmpty()
-      if (townsEmpty || citiesEmpty) {
-        if (townsEmpty) setTownsError('Debe agregar al menos un municipio')
-        if (citiesEmpty) setCitiesError('Debe agregar al menos una ciudad')
-        return
-      }
-  
-      const formattedPayload = formatPayloadCreateAddress({ payload: data, cities, towns, isGEAddress: shouldCreateGEAddress })
-      if (isEdit) {
-        editAddressMutation(formattedPayload)
-        return
-      }
-  
-      if (!shouldCreateGEAddress && !consentSkipGECreation) {
-        setShowConsentError(true)
-        return
-      }
-  
-      // If the address is created in GE, then the create address mutation will be executed in that screen
-      if (shouldCreateGEAddress && !consentSkipGECreation) {
-        // Check if alias exists in GE
-        if ((dataAliases ?? []).find(aliasFetched => aliasFetched === data.alias)) {
-          setError('alias', {
-            type: 'manual',
-            message: 'El alias ya existe en GE, por favor elija otro'
-          })
-          return;
-        }
+    event?.preventDefault();
+    if (showConsentError) setShowConsentError(false);
 
-        const GEpayload = convertToAddressDataGEFormValues(data, cities)
-        updateAddressDataGE(GEpayload)
-        setSubscreen('ADD_GE_INFORMATION')
-      }
-
-      // Create the address anyway
-      createAddressMutation(formattedPayload)
+    // Check if alias has been modified in edit mode
+    if (isEdit && formData?.alias && data?.alias !== formData.alias) {
+      setError("alias", {
+        type: "manual",
+        message: "El alias no puede ser editado",
+      });
+      return;
     }
+
+    const townsEmpty = validateTownsEmpty();
+    const citiesEmpty = validateCitiesEmpty();
+    if (townsEmpty || citiesEmpty) {
+      if (townsEmpty) setTownsError("Debe agregar al menos un municipio");
+      if (citiesEmpty) setCitiesError("Debe agregar al menos una ciudad");
+      return;
+    }
+
+    const formattedPayload = formatPayloadCreateAddress({
+      payload: data,
+      cities,
+      towns,
+      isGEAddress: shouldCreateGEAddress,
+    });
+    if (isEdit) {
+      editAddressMutation(formattedPayload);
+      return;
+    }
+
+    if (!shouldCreateGEAddress && !consentSkipGECreation) {
+      setShowConsentError(true);
+      return;
+    }
+
+    // If the address is created in GE, then the create address mutation will be executed in that screen
+    if (shouldCreateGEAddress && !consentSkipGECreation) {
+      // Check if alias exists in GE
+      if (
+        (dataAliases ?? []).find((aliasFetched) => aliasFetched === data.alias)
+      ) {
+        setError("alias", {
+          type: "manual",
+          message: "El alias ya existe en GE, por favor elija otro",
+        });
+        return;
+      }
+
+      const GEpayload = convertToAddressDataGEFormValues(data, cities);
+      updateAddressDataGE(GEpayload);
+      setSubscreen("ADD_GE_INFORMATION");
+    }
+
+    // Create the address anyway
+    createAddressMutation(formattedPayload);
+  };
+
+  const [tempZipcode, setTempZipcode] = useState<string>("");
 
   return (
     <div className="flex flex-col gap-5">
-      { (errorFetchAlias && showErrorBanner) && (
-        <ErrorBanner message={errorFetchAlias} toggleError={toggleErrorBanner} />
+      {errorFetchAlias && showErrorBanner && (
+        <ErrorBanner
+          message={errorFetchAlias}
+          toggleError={toggleErrorBanner}
+        />
       )}
       <form
         className="grid grid-cols-1 lg:grid-cols-2 gap-5"
@@ -196,7 +234,7 @@ export const CreateAddressSubform = ({
             type="text"
             {...register("street1")}
           />
-          { errors?.street1?.message && (
+          {errors?.street1?.message && (
             <ErrorMessage>{errors.street1?.message}</ErrorMessage>
           )}
         </div>
@@ -212,7 +250,7 @@ export const CreateAddressSubform = ({
             inputMode="numeric"
             {...register("externalNumber")}
           />
-          { errors?.externalNumber?.message && (
+          {errors?.externalNumber?.message && (
             <ErrorMessage>{errors.externalNumber?.message}</ErrorMessage>
           )}
         </div>
@@ -228,7 +266,7 @@ export const CreateAddressSubform = ({
             inputMode="numeric"
             {...register("internalNumber")}
           />
-          { errors?.internalNumber?.message && (
+          {errors?.internalNumber?.message && (
             <ErrorMessage>{errors.internalNumber?.message}</ErrorMessage>
           )}
         </div>
@@ -243,7 +281,7 @@ export const CreateAddressSubform = ({
             type="text"
             {...register("neighborhood")}
           />
-          { errors?.neighborhood?.message && (
+          {errors?.neighborhood?.message && (
             <ErrorMessage>{errors.neighborhood?.message}</ErrorMessage>
           )}
         </div>
@@ -278,7 +316,7 @@ export const CreateAddressSubform = ({
             type="text"
             {...register("state")}
           />
-          { errors?.state?.message && (
+          {errors?.state?.message && (
             <ErrorMessage>{errors.state?.message}</ErrorMessage>
           )}
         </div>
@@ -294,7 +332,7 @@ export const CreateAddressSubform = ({
             inputMode="numeric"
             {...register("zipcode")}
           />
-          { errors?.zipcode?.message && (
+          {errors?.zipcode?.message && (
             <ErrorMessage>{errors.zipcode?.message}</ErrorMessage>
           )}
         </div>
@@ -309,7 +347,7 @@ export const CreateAddressSubform = ({
             type="text"
             {...register("reference")}
           />
-          { errors?.reference?.message && (
+          {errors?.reference?.message && (
             <ErrorMessage>{errors.reference?.message}</ErrorMessage>
           )}
         </div>
@@ -324,35 +362,51 @@ export const CreateAddressSubform = ({
             type="text"
             {...register("alias")}
           />
-          { errors?.alias?.message && (
+          {errors?.alias?.message && (
             <ErrorMessage>{errors.alias?.message}</ErrorMessage>
           )}
         </div>
-        <ToggleSwitch checked={shouldCreateGEAddress} label="Crear dirección en GE" onChange={handleShouldCreateGEAddress} />
-        { !shouldCreateGEAddress && (
+        <ToggleSwitch
+          checked={shouldCreateGEAddress}
+          label="Crear dirección en GE"
+          onChange={handleShouldCreateGEAddress}
+        />
+        {!shouldCreateGEAddress && (
           <>
             <div className="flex items-center gap-2">
-              <Checkbox 
+              <Checkbox
                 id="remember"
                 checked={consentSkipGECreation}
                 onChange={(e) => handleConsentSkipGECreation(e.target.checked)}
               />
-              <Label htmlFor="remember">Entiendo y acepto omitir en no crear esta dirección en GE</Label>
+              <Label htmlFor="remember">
+                Entiendo y acepto omitir en no crear esta dirección en GE
+              </Label>
             </div>
-            { showConsentError && (
+            {showConsentError && (
               <div className="lg:col-start-2 lg:col-end-3 w-full flex justify-center">
                 <ErrorMessage>Marque esta opcion para continuar.</ErrorMessage>
               </div>
             )}
           </>
         )}
+        <AutocompleteZipcode
+          zipcode={tempZipcode}
+          setZipcode={setTempZipcode}
+        />
         <div className="lg:col-span-2 flex justify-between mt-4">
           <Button
             color="red"
             outline
             data-testid="origin-address-cancel-button"
             className="hover:cursor-pointer"
-            disabled={isPending || isSuccess || isPendingEdit || isSuccessEdit || isPendingFetchAlias}
+            disabled={
+              isPending ||
+              isSuccess ||
+              isPendingEdit ||
+              isSuccessEdit ||
+              isPendingFetchAlias
+            }
             onClick={toggleModal}
           >
             Cancelar
@@ -363,12 +417,18 @@ export const CreateAddressSubform = ({
             className="hover:cursor-pointer"
             disabled={isPending || isSuccess || isPendingEdit || isSuccessEdit}
           >
-            { (isSuccess || isSuccessEdit) && (<CheckIcon />)}
-            { (isPending || isPendingEdit) && (<Spinner aria-label={`loading ${actionText} kraft envios`} />) }
-            { !isSuccess && !isSuccessEdit && !isPending && !isPendingEdit && submitButtonText }
+            {(isSuccess || isSuccessEdit) && <CheckIcon />}
+            {(isPending || isPendingEdit) && (
+              <Spinner aria-label={`loading ${actionText} kraft envios`} />
+            )}
+            {!isSuccess &&
+              !isSuccessEdit &&
+              !isPending &&
+              !isPendingEdit &&
+              submitButtonText}
           </Button>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
