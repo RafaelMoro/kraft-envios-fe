e066c0ec4f2e9b01a0a4fefbb02894833b696895
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-01-25 16:44:00 -0600

    feat: add NeighborhoodDropdown component for improved neighborhood selection

--- src/features/Addresses/AutocompleteZipcode.tsx
+++ src/features/Addresses/AutocompleteZipcode.tsx
@@ -26,6 +26,7 @@ import {
   getAddressByZipcode,
   selectAddressValue,
 } from "@/shared/utils/addresses.utils";
+import { NeighborhoodDropdown } from "../AutocompleteZipcode/NeighborhoodDropdown";
 
 interface AutocompleteZipcodeProps {
   hideCityField?: boolean;
@@ -198,41 +199,13 @@ export const AutocompleteZipcode = ({
         />
         {zipcodeError && <ErrorMessage>{zipcodeError}</ErrorMessage>}
       </div>
-      <Dropdown
-        label=""
-        renderTrigger={() => (
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="neighborhood">Colonia</Label>
-            </div>
-            <Button
-              className="hover:cursor-pointer flex justify-between w-full"
-              data-testid="autocomplete-dropdown-neighborhood-button"
-              color="light"
-              disabled={isFetching || neighborhoods.length === 0}
-            >
-              {isFetching ? <Spinner /> : neighborhood}
-              <RiArrowDownSLine />
-            </Button>
-            {neighborhoodError && (
-              <ErrorMessage>{neighborhoodError}</ErrorMessage>
-            )}
-          </div>
-        )}
-      >
-        <div className="overflow-y-auto max-h-52">
-          {neighborhoods &&
-            neighborhoods.length > 0 &&
-            neighborhoods.map((item) => (
-              <DropdownItem
-                key={`neighborhood-${item}`}
-                onClick={() => setNeighborhood(item)}
-              >
-                {item}
-              </DropdownItem>
-            ))}
-        </div>
-      </Dropdown>
+      <NeighborhoodDropdown
+        isFetching={isFetching}
+        neighborhood={neighborhood}
+        neighborhoods={neighborhoods}
+        neighborhoodError={neighborhoodError}
+        setNeighborhood={setNeighborhood}
+      />
       <Dropdown
         label=""
         renderTrigger={() => (
