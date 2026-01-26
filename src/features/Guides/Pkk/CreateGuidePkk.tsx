2d8c9b0637201a5daabe2fde9cc06e31709d0550
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-01-26 12:24:39 -0600

    remove console log

--- src/features/Guides/Pkk/CreateGuidePkk.tsx
+++ src/features/Guides/Pkk/CreateGuidePkk.tsx
@@ -32,7 +32,6 @@ export const CreateGuidePkk = ({
   const { aliasesPkk, updateOriginAliasPkk, updateDestinationAliasPkk, resetAliases } = useSaveAlias()
 
   const formData = useRef<CreateGuideFormValuesPkk>({...initialStateFormPkk})
-  console.log('formData', formData.current)
   const resetFormData = () => {
     formData.current = {...initialStateFormPkk}
   }
