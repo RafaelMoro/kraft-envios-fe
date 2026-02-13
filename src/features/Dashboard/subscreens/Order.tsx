b12c76848c72a324854c652defc6d88c65edae7a
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-02-13 13:05:08 -0600

    fetch guides data

--- src/features/Dashboard/subscreens/Order.tsx
+++ src/features/Dashboard/subscreens/Order.tsx
@@ -1,11 +1,20 @@
-import { LoginData } from "@/shared/types/login.types"
 import { RiBuilding2Line } from "@remixicon/react"
+import { useQuery } from "@tanstack/react-query"
+
+import { LoginData } from "@/shared/types/login.types"
+import { getGuidesCb } from "@/shared/utils/guides.utils"
 
 interface OrderProps {
   userInfo: LoginData | null
 }
 
 export const Order = ({ userInfo }: OrderProps) => {
+  const { data, refetch, isPending, isError } = useQuery({
+    queryKey: ['guides'],
+    queryFn: getGuidesCb
+  })
+  console.log('data', data)
+
   return (
     <main className='p-4 flex flex-col gap-5 justify-center items-center w-full'>
       <h1 className="text-3xl font-bold text-center">Bienvenido {userInfo?.data?.user?.name}</h1>
