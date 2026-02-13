1d5eeeffebf62b6981ce6f7544fb1d6025b842aa
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-02-13 16:44:59 -0600

    feat: refactor GuideCard component for improved structure and styling

--- src/features/Guides/ViewGuides/GuideCard.tsx
+++ src/features/Guides/ViewGuides/GuideCard.tsx
@@ -9,22 +9,38 @@ interface GuideCardProps {
 export const GuideCard = ({ guide }: GuideCardProps) => {
   return (
     <Card href="#" className="max-w-sm">
-      <Badge color="success">{guide.status}</Badge>
+      { /** Preheader */}
+      <div className="flex justify-between">
+        <Badge color="success">{guide.status}</Badge>
+        <p className="text-sm font-bold">Logo pending</p>
+      </div>
+
+      { /** Header */}
       <div>
-        <span>Número de Guia</span>
-        <h1>{guide.trackingNumber}</h1>
+        <span className="text-gray-600 dark:text-gray-400 text-sm">Número de Guia</span>
+        <h1 className="text-xl font-semibold">{guide.trackingNumber}</h1>
         <div>
-          <span>Envío: #{guide?.shipmentNumber}</span>
+          <span className="text-gray-600 dark:text-gray-400 text-sm">Envío: {guide?.shipmentNumber}</span>
         </div>
       </div>
-      <div>
-        <div>
-          <RiHome9Line />
-          <p>Remitente</p>
+
+      { /** Address info */}
+      <div className="grid grid-cols-2 mt-5">
+        <div className="text-gray-600 dark:text-gray-400">
+          <div className="inline-flex gap-2">
+            <RiHome9Line size={18} />
+            <p className="text-xs">Remitente</p>
+          </div>
+          <p className="font-semibold text-ellipsis">{guide.origin.name}</p>
+          <p className="text-xs text-gray-600 dark:text-gray-400">{guide.origin.city}</p>
         </div>
-        <div>
-          <RiHome9Line />
-          <p>Destinatario</p>
+        <div className="text-gray-600 dark:text-gray-400">
+          <div className="inline-flex gap-2">
+            <RiHome9Line size={18} />
+            <p className="text-xs">Destinatario</p>
+          </div>
+          <p className="font-semibold text-ellipsis">{guide.destination.name}</p>
+          <p className="text-xs text-gray-600 dark:text-gray-400">{guide.destination.city}</p>
         </div>
       </div>
     </Card>
