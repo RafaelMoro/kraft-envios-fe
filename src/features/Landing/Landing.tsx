d1ff34519a4b19957c45f9b321dbc8ede8dd1cf1
Rafael Moro G <rafael.moro.galindo@gmail.com>
2026-07-25 21:41:11 -0600

    Add LandingFaq and RevealOnScroll components to the landing page

--- src/features/Landing/Landing.tsx
+++ src/features/Landing/Landing.tsx
@@ -6,21 +6,38 @@ import { MidPageCta } from '@/features/Landing/MidPageCta'
 import { PlatformFeatures } from '@/features/Landing/PlatformFeatures'
 import { AudienceGrid } from '@/features/Landing/AudienceGrid'
 import { CourierPanel } from '@/features/Landing/CourierPanel'
+import { LandingFaq } from '@/features/Landing/LandingFaq'
 import { LandingCta } from '@/features/Landing/LandingCta'
 import { LandingFooter } from '@/features/Landing/LandingFooter'
+import { RevealOnScroll } from '@/features/Landing/RevealOnScroll'
 
 export const Landing = () => (
   <div className="min-h-screen bg-gray-50 text-gray-900 font-[family-name:var(--font-geist-sans)]">
     <LandingHeader />
     <main>
       <LandingHero />
-      <ValueBar />
-      <HowItWorks />
-      <MidPageCta />
-      <PlatformFeatures />
-      <AudienceGrid />
-      <CourierPanel />
-      <LandingCta />
+      <RevealOnScroll>
+        <ValueBar />
+      </RevealOnScroll>
+      <RevealOnScroll>
+        <HowItWorks />
+      </RevealOnScroll>
+      <RevealOnScroll>
+        <MidPageCta />
+      </RevealOnScroll>
+      <RevealOnScroll>
+        <PlatformFeatures />
+      </RevealOnScroll>
+      <RevealOnScroll>
+        <AudienceGrid />
+      </RevealOnScroll>
+      <RevealOnScroll>
+        <CourierPanel />
+      </RevealOnScroll>
+      <LandingFaq />
+      <RevealOnScroll>
+        <LandingCta />
+      </RevealOnScroll>
     </main>
     <LandingFooter />
   </div>
